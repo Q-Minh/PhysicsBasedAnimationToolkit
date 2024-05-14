ab7a5e1853f2a483f05cd1dbcb8d00457f8c1a09
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2024-05-14 15:30:38 -0400

    Implement mass matrix construction

--- include/pba/fem/MassMatrix.h
+++ include/pba/fem/MassMatrix.h
@@ -2,6 +2,8 @@
 #define PBA_CORE_FEM_MASS_MATRIX_H
 
 #include "Concepts.h"
+#include "pba/common/Eigen.h"
+#include "pba/math/SymmetricQuadratureRules.h"
 
 namespace pba {
 namespace fem {
@@ -10,15 +12,19 @@ template <CMesh TMesh, int Dims>
 struct MassMatrix
 {
   public:
-    using MeshType              = TMesh;
+    using MeshType    = TMesh;
+    using ElementType = typename TMesh::ElementType;
+    using QuadratureRuleType =
+        math::SymmetricPolynomialQuadratureRule<ElementType::kDims, 2 * ElementType::kOrder>;
     static int constexpr kDims  = Dims;
-    using ElementType           = typename TMesh::ElementType;
     static int constexpr kOrder = 2 * ElementType::kOrder;
 
     MassMatrix(MeshType const& mesh, Scalar rho = 1.);
 
     template <class Derived>
     MassMatrix(MeshType const& mesh, Eigen::DenseBase<Derived> const& rho);
+
+    void ComputeElementMassMatrices(MeshType const& mesh);
 
     VectorX rho; ///< |#elements| x 1 piecewise constant mass density
     MatrixX Me;  ///< |ElementType::Nodes|x|ElementType::Nodes * |#elements| element mass matrices
@@ -29,6 +35,7 @@ inline MassMatrix<TMesh, Dims>::MassMatrix(MeshType const& mesh, Scalar rho)
     : rho(VectorX::Constant(rho, mesh.E.cols())),
       Me(ElementType::kNodes, ElementType::kNodes * mesh.E.cols())
 {
+    ComputeElementMassMatrices();
 }
 
 template <CMesh TMesh, int Dims>
@@ -38,8 +45,42 @@ inline MassMatrix<TMesh, Dims>::MassMatrix(
     Eigen::DenseBase<Derived> const& rho)
     : rho(rho), Me(ElementType::kNodes, ElementType::kNodes * mesh.E.cols())
 {
+    ComputeElementMassMatrices();
+}
+
+template <CMesh TMesh, int Dims>
+inline void MassMatrix<TMesh, Dims>::ComputeElementMassMatrices(MeshType const& mesh)
+{
+    Me.setZero();
+    // WARNING: We currently do not have quadrature rules for non-simplex elements, i.e. hexahedra
+    // and quadrilaterals. The QuadratureRuleType type member should be declared in the element's
+    // type, taking only the polynomial order as an argument. This way, the mass matrix can
+    // integrate any element using its associated quadrature rule as:
+    //
+    // using QuadratureRuleType = ElementType::QuadratureRuleType<kOrder>;
+    //
+    auto const Xg = common::ToEigen(QuadratureRuleType::points)
+                        .reshaped(QuadratureRuleType::kDims + 1, QuadratureRuleType::kPoints)
+                        .bottomRows(QuadratureRuleType::kDims);
+    auto const numberOfElements = mesh.E.cols();
+    for (auto e = 0; e < numberOfElements; ++e)
+    {
+        auto me = Me.block(0, e * ElementType::kNodes, ElementType::kNodes, ElementType::kNodes);
+        for (auto g = 0; g < Q.kPoints; ++g)
+        {
+            Vector<ElementType::kNodes> const Ng = ElementType::N(Xg.col(g));
+            for (auto j = 0; j < ElementType::kNodes; ++j)
+            {
+                for (auto i = 0; i < ElementType::kNodes; ++i)
+                {
+                    me(i, j) += rho(e) * Ng(i) * Ng(j);
+                }
+            }
+        }
+    }
 }
 
 } // namespace fem
 } // namespace pba
+
 #endif // PBA_CORE_FEM_MASS_MATRIX_H
