89529dbc786b6914c6bb18ee5fafa87d57a6f3f4
Q-Minh <tonthat.quocminh@gmail.com>
2024-05-21 13:15:10 -0400

    Use consistent naming convention

--- include/pba/fem/DeformationGradient.h
+++ include/pba/fem/DeformationGradient.h
@@ -23,13 +23,13 @@ Matrix<TElement::kNodes, TDerivedX::RowsAtCompileTime> BasisFunctionGradients(
     // [ grad_X \phi(X) ]^T = J^{-T} * grad_\Xi N^T
     // Recall that the pseudoinverse of J is J^{-1} = U \Sigma^{-1} V^T
     // We pseudoinvert its transpose directly, J^{-T} = V \Sigma^{-1} U^T
-    auto constexpr InputDims                     = TElement::kDims;
-    auto constexpr OutputDims                    = TDerivedX::RowsAtCompileTime;
-    using AffineElementType                      = typename TElement::AffineBaseType;
-    Matrix<TElement::kNodes, InputDims> const GN = TElement::GradN(Xi);
-    Matrix<InputDims, OutputDims> const JT       = Jacobian<AffineElementType>(Xi, X).transpose();
+    auto constexpr kInputDims                     = TElement::kDims;
+    auto constexpr kOutputDims                    = TDerivedX::RowsAtCompileTime;
+    using AffineElementType                       = typename TElement::AffineBaseType;
+    Matrix<TElement::kNodes, kInputDims> const GN = TElement::GradN(Xi);
+    Matrix<kInputDims, kOutputDims> const JT      = Jacobian<AffineElementType>(Xi, X).transpose();
     auto JinvT = JT.jacobiSvd(Eigen::ComputeFullU | Eigen::ComputeFullV);
-    Matrix<TElement::kNodes, OutputDims> const GP = JinvT.solve(GN.transpose()).transpose();
+    Matrix<TElement::kNodes, kOutputDims> const GP = JinvT.solve(GN.transpose()).transpose();
     return GP;
 }
 
