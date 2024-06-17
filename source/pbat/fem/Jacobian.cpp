a120842a1353f275bb983c004633dd7cd0c4380e
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2024-06-17 14:03:02 -0400

    Implement domain to reference space map

--- source/pbat/fem/Jacobian.cpp
+++ source/pbat/fem/Jacobian.cpp
@@ -22,18 +22,65 @@ TEST_CASE("[fem] Jacobian")
             3, 0, 6, 5, 3,
             5, 6, 0, 3, 6;
     // clang-format on
-    common::ForRange<1, 4>([&]<auto PolynomialOrder>() {
-        auto constexpr kDims = 3;
-        using Element        = fem::Tetrahedron<PolynomialOrder>;
-        using Mesh           = fem::Mesh<Element, kDims>;
-        Mesh const mesh{V, C};
-        common::ForRange<1, 4>([&]<auto QuadratureOrder>() {
-            MatrixX const detJe = fem::DeterminantOfJacobian<QuadratureOrder>(mesh);
-            CHECK_EQ(detJe.rows(), Element::template QuadratureType<QuadratureOrder>::kPoints);
-            auto const numberOfElements = mesh.E.cols();
-            CHECK_EQ(detJe.cols(), numberOfElements);
-            bool const bIsStrictlyPositive = (detJe.array() > 0.).all();
-            CHECK(bIsStrictlyPositive);
+    SUBCASE("Jacobian determinants")
+    {
+        common::ForRange<1, 4>([&]<auto PolynomialOrder>() {
+            auto constexpr kDims = 3;
+            using Element        = fem::Tetrahedron<PolynomialOrder>;
+            using Mesh           = fem::Mesh<Element, kDims>;
+            Mesh const mesh{V, C};
+            common::ForRange<1, 4>([&]<auto QuadratureOrder>() {
+                MatrixX const detJe = fem::DeterminantOfJacobian<QuadratureOrder>(mesh);
+                CHECK_EQ(detJe.rows(), Element::template QuadratureType<QuadratureOrder>::kPoints);
+                auto const numberOfElements = mesh.E.cols();
+                CHECK_EQ(detJe.cols(), numberOfElements);
+                bool const bIsStrictlyPositive = (detJe.array() > 0.).all();
+                CHECK(bIsStrictlyPositive);
+            });
         });
-    });
+    }
+    SUBCASE("Computing map from domain to reference space")
+    {
+        common::ForRange<1, 4>([&]<auto PolynomialOrder>() {
+            auto constexpr kDims = 3;
+            using Element        = fem::Tetrahedron<PolynomialOrder>;
+            using Mesh           = fem::Mesh<Element, kDims>;
+            Mesh const mesh{V, C};
+
+            // Setup expected reference positions
+            Matrix<Element::kDims, Element::kNodes> const XiExpected =
+                common::ToEigen(Element::Coordinates)
+                    .reshaped(Element::kDims, Element::kNodes)
+                    .cast<Scalar>() /
+                static_cast<Scalar>(Element::kOrder);
+
+            // Compute reference positions
+            IndexVectorX E(mesh.E.cols() * Element::kNodes);
+            MatrixX X(kDims, E.size());
+            for (auto e = 0; e < mesh.E.cols(); ++e)
+            {
+                E(Eigen::seqN(e * Element::kNodes, Element::kNodes)).array() = e;
+                auto const nodes                                             = mesh.E.col(e);
+                X.block<Mesh::kDims, Element::kNodes>(0, e * Element::kNodes) =
+                    mesh.X(Eigen::all, nodes);
+            }
+
+            auto constexpr maxIterations = 2;
+            Scalar constexpr eps         = 1e-10;
+            MatrixX const XiComputed     = fem::ReferencePositions(mesh, E, X, maxIterations, eps);
+
+            // Assert
+            CHECK_EQ(XiComputed.rows(), Element::kDims);
+            CHECK_EQ(XiComputed.cols(), E.size());
+            for (auto e = 0; e < mesh.E.cols(); ++e)
+            {
+                auto const nodes = mesh.E.col(e);
+                Matrix<Element::kDims, Element::kNodes> const XiComputedBlock =
+                    XiComputed.block<Element::kDims, Element::kNodes>(0, e * Element::kNodes);
+                Matrix<Element::kDims, Element::kNodes> const error = XiComputedBlock - XiExpected;
+                Scalar const errorMagnitude                         = error.squaredNorm();
+                CHECK_LE(errorMagnitude, eps);
+            }
+        });
+    }
 }
--- source/pbat/fem/Jacobian.h
+++ source/pbat/fem/Jacobian.h
@@ -3,6 +3,7 @@
 
 #include "Concepts.h"
 
+#include <Eigen/Cholesky>
 #include <Eigen/LU>
 #include <Eigen/SVD>
 #include <pbat/Aliases.h>
@@ -74,6 +75,87 @@ MatrixX DeterminantOfJacobian(TMesh const& mesh)
         }
     });
     return detJe;
+}
+
+template <CElement TElement, class TDerivedX, class TDerivedx>
+Vector<TElement::kDims> ReferencePosition(
+    Eigen::MatrixBase<TDerivedX> const& X,
+    Eigen::MatrixBase<TDerivedx> const& x,
+    int const maxIterations = 5,
+    Scalar const eps        = 1e-10)
+{
+    // We need to solve the inverse problem \argmin_\Xi f(\Xi) = 1/2 ||x(\Xi) - X||_2^2
+    // to find the reference position \Xi that corresponds to the domain position X in the element
+    // whose vertices are x.
+
+    // We use Gauss-Newton iterations on \argmin f.
+    // This gives the iteration dx^k = [H(\Xi^k)]^{-1} J_x(\Xi^k)^T (x(\Xi^k) - X),
+    // where H(\Xi^k) = J_x(\Xi^k)^T J_x(\Xi^k).
+
+    using ElementType = TElement;
+    assert(x.cols() == ElementType::kNodes);
+    auto constexpr kDims    = ElementType::kDims;
+    auto constexpr kNodes   = ElementType::kNodes;
+    auto constexpr kOutDims = Eigen::MatrixBase<TDerivedx>::RowsAtCompileTime;
+    // Initial guess is element's barycenter.
+    auto const coords = common::ToEigen(ElementType::Coordinates).reshaped(kDims, kNodes);
+    auto const vertexLagrangePositions =
+        (coords(Eigen::all, ElementType::Vertices).cast<Scalar>() /
+         static_cast<Scalar>(ElementType::kOrder));
+    Vector<kDims> Xik =
+        vertexLagrangePositions.rowwise().sum() / static_cast<Scalar>(ElementType::Vertices.size());
+
+    Vector<kOutDims> rk = x * ElementType::N(Xik) - X;
+    Matrix<kOutDims, kDims> J;
+    Eigen::LDLT<Matrix<kDims, kDims>> LDLT;
+
+    // If Jacobian is constant, let's precompute it
+    if constexpr (ElementType::bHasConstantJacobian)
+    {
+        J = Jacobian<ElementType>(Xik, x);
+        LDLT.compute(J.transpose() * J);
+    }
+    // Do up to maxIterations Gauss Newton iterations
+    for (auto k = 0; k < maxIterations; ++k)
+    {
+        if (rk.lpNorm<1>() <= eps)
+            break;
+
+        // Non-constant jacobians need to be updated
+        if constexpr (not ElementType::bHasConstantJacobian)
+        {
+            J = Jacobian<ElementType>(Xik, x);
+            LDLT.compute(J.transpose() * J);
+        }
+
+        Xik -= LDLT.solve(J.transpose() * rk);
+        rk = x * ElementType::N(Xik) - X;
+    }
+    return Xik;
+}
+
+template <CMesh TMesh, class TDerivedE, class TDerivedX>
+MatrixX ReferencePositions(
+    TMesh const& mesh,
+    Eigen::DenseBase<TDerivedE> const& E,
+    Eigen::MatrixBase<TDerivedX> const& X,
+    int maxIterations = 5,
+    Scalar eps        = 1e-10)
+{
+    PBA_PROFILE_SCOPE;
+    using MeshType    = TMesh;
+    using ElementType = typename MeshType::ElementType;
+    MatrixX Xi(ElementType::kDims, E.size());
+    tbb::parallel_for(Index{0}, Index{E.size()}, [&](Index k) {
+        Index const e                     = E(k);
+        auto const nodes                  = mesh.E.col(e);
+        auto constexpr kOutDims           = MeshType::kDims;
+        auto constexpr kNodes             = ElementType::kNodes;
+        Matrix<kOutDims, kNodes> const Xe = mesh.X(Eigen::all, nodes);
+        Vector<kOutDims> const Xk         = X.col(k);
+        Xi.col(k) = ReferencePosition<ElementType>(Xk, Xe, maxIterations, eps);
+    });
+    return Xi;
 }
 
 } // namespace fem
