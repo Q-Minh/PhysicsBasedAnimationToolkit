deb1822592d32e858aadd519bfb22ca1ff00b04d
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2024-06-27 13:48:26 -0400

    Showcase diffusion based mesh smoothing

---
+++ python/examples/diffusion_smoothing.py
@@ -0,0 +1,67 @@
+import pbatoolkit as pbat
+import pbatoolkit.fem
+import pbatoolkit.math.linalg
+import igl
+import polyscope as ps
+import polyscope.imgui as imgui
+import numpy as np
+import scipy as sp
+import argparse
+import meshio
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Diffusion mesh smoothing demo",
+    )
+    parser.add_argument("-i", "--input", help="Path to input triangle mesh", type=str,
+                        dest="input", required=True)
+    args = parser.parse_args()
+    
+    imesh = meshio.read(args.input)
+    V, F = imesh.points, imesh.cells_dict["triangle"]
+    V, F = V.astype(np.float64, order='c'), F.astype(np.int64, order='c')
+    
+    # Construct Galerkin laplacian, mass and gradient operators
+    mesh = pbat.fem.mesh(
+        V.T, F.T, element=pbat.fem.Element.Triangle, order=1)
+    V, F = mesh.X.T, mesh.E.T
+    detJeL = pbat.fem.jacobian_determinants(mesh, quadrature_order=1)
+    GNeL = pbat.fem.shape_function_gradients(mesh, quadrature_order=1)
+    L = pbat.fem.laplacian_matrix(
+        mesh, detJeL, GNeL, quadrature_order=1).to_matrix()
+    detJeM = pbat.fem.jacobian_determinants(mesh, quadrature_order=2)
+    M = pbat.fem.mass_matrix(mesh, detJeM, dims=1,
+                             quadrature_order=2).to_matrix()
+    # Setup heat diffusion
+    dt = 0.016
+    c = 1
+    A = M - c*dt*L
+    # Precompute linear solvers
+    Ainv = pbat.math.linalg.ldlt(A)
+    Ainv.compute(A)
+
+    ps.set_up_dir("z_up")
+    ps.set_front_dir("neg_y_front")
+    ps.set_ground_plane_mode("shadow_only")
+    ps.init()
+    vmm = ps.register_surface_mesh("model", V, F)
+    vmm.set_smooth_shade(True)
+    vms = ps.register_surface_mesh("smoothed", V, F)
+    vms.set_smooth_shade(True)
+    smooth = False
+    
+    def callback():
+        global dt, Ainv, M, L, smooth, V, c
+        dtchanged, dt = imgui.InputFloat("dt", dt)
+        cchanged, c = imgui.SliderFloat("c", c, v_min=0, v_max=100)
+        if dtchanged or cchanged:
+            A = M - c*dt*L
+            Ainv.factorize(A)
+        _, smooth = imgui.Checkbox("smooth", smooth)
+        if smooth:
+            V = Ainv.solve(M @ V)
+            vms.update_vertex_positions(V)
+            
+            
+    ps.set_user_callback(callback)
+    ps.show()
