e94932de50901da054f4e0210720c29c316ffbe5
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2024-06-27 13:48:13 -0400

    Improve laplace interpolation demo

--- python/examples/laplace.py
+++ python/examples/laplace.py
@@ -2,12 +2,14 @@ import pbatoolkit as pbat
 import pbatoolkit.fem
 import pbatoolkit.math.linalg
 import pbatoolkit.geometry
+import igl
 import meshio
 import polyscope as ps
 import numpy as np
 import argparse
 
-def harmonic_field(V: np.ndarray, C: np.ndarray, order: int):
+
+def harmonic_field(V: np.ndarray, C: np.ndarray, order: int, eps: float = 0.1):
     # Construct order order mesh and its Laplacian
     mesh = pbat.fem.mesh(
         V.T, C.T, element=pbat.fem.Element.Tetrahedron, order=order)
@@ -21,20 +23,17 @@ def harmonic_field(V: np.ndarray, C: np.ndarray, order: int):
     # Set Dirichlet boundary conditions at bottom and top of the model
     Xmin = mesh.X.min(axis=1)
     Xmax = mesh.X.max(axis=1)
-    dXmin = np.zeros(mesh.dims)
-    dXmax = np.zeros(mesh.dims)
-    eps = 1e-4
-    dXmin[-1] = -eps
-    dXmax[-1] = -(Xmax[-1] - Xmin[-1]) + eps
-    aabblo = pbat.geometry.aabb(np.vstack((Xmin + dXmin, Xmax + dXmax)).T)
+    extents = Xmax - Xmin
+    extents[:-1] = 0
+    aabblo = pbat.geometry.aabb(
+        np.vstack((Xmin - eps*extents, Xmax - (1-eps)*extents)).T)
     gammalo = aabblo.contained(mesh.X)
-    dXmin[-1] = (Xmax[-1] - Xmin[-1]) - eps
-    dXmax[-1] = eps
-    aabbhi = pbat.geometry.aabb(np.vstack((Xmin + dXmin, Xmax + dXmax)).T)
+    aabbhi = pbat.geometry.aabb(
+        np.vstack((Xmin + (1-eps)*extents, Xmax + eps*extents)).T)
     gammahi = aabbhi.contained(mesh.X)
     gamma = np.concatenate((gammalo, gammahi))
     uk = np.concatenate((np.ones(len(gammalo)), np.zeros(len(gammahi))))
-    
+
     # Solve boundary value problem
     n = mesh.X.shape[1]
     dofs = np.setdiff1d(list(range(n)), gamma)
@@ -60,7 +59,7 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--refined-input", help="Path to refined input tetrahedral mesh", type=str,
                         dest="rinput", required=True)
     args = parser.parse_args()
-    
+
     imesh = meshio.read(args.input)
     V, C, = imesh.points, imesh.cells_dict["tetra"]
     V = np.copy(V, order='c')
@@ -69,22 +68,34 @@ if __name__ == "__main__":
     u2, mesh2 = harmonic_field(V, C, order=2)
     bvh = pbat.geometry.bvh(V.T, C.T, cell=pbat.geometry.Cell.Tetrahedron)
     rmesh = meshio.read(args.rinput)
-    Vrefined, Crefined = rmesh.points, rmesh.cells_dict["tetra"]
-    
+    Vrefined, Crefined = rmesh.points.astype(
+        np.float64, order='c'), rmesh.cells_dict["tetra"].astype(np.int64, order='c')
+    Frefined = igl.boundary_facets(Crefined)
+    Frefined[:,:2] = np.roll(Frefined[:,:2], shift=1, axis=1)
+
     e = bvh.nearest_primitives_to_points(Vrefined.T)
     Xi1 = pbat.fem.reference_positions(mesh1, e, Vrefined.T)
     Xi2 = pbat.fem.reference_positions(mesh2, e, Vrefined.T)
     phi1 = pbat.fem.shape_functions_at(mesh1, Xi1)
     phi2 = pbat.fem.shape_functions_at(mesh2, Xi2)
-    u1ref = (u1[mesh1.E[:,e]] * phi1).sum(axis=0)
-    u2ref = (u2[mesh2.E[:,e]] * phi2).sum(axis=0)
-    
+    u1ref = (u1[mesh1.E[:, e]] * phi1).sum(axis=0)
+    u2ref = (u2[mesh2.E[:, e]] * phi2).sum(axis=0)
+
     ps.set_up_dir("z_up")
     ps.set_front_dir("neg_y_front")
     ps.set_ground_plane_mode("shadow_only")
     ps.init()
-    ps.register_volume_mesh("domain", V, C)
     vm = ps.register_volume_mesh("domain refined", Vrefined, Crefined)
-    vm.add_scalar_quantity("Order 1 harmonic solution", u1ref)
-    vm.add_scalar_quantity("Order 2 harmonic solution", u2ref)
+    vm.add_scalar_quantity("Order 1 harmonic solution", u1ref, enabled=True, cmap="turbo")
+    vm.add_scalar_quantity("Order 2 harmonic solution", u2ref, cmap="turbo")
+    niso = 15
+    Viso1, Eiso1 = igl.isolines(Vrefined, Frefined, u1ref, niso)
+    Viso2, Eiso2 = igl.isolines(Vrefined, Frefined, u2ref, niso)
+    cn1 = ps.register_curve_network("Order 1 contours", Viso1, Eiso1)
+    cn1.set_radius(0.002)
+    cn1.set_color((0, 0, 0))
+    cn2 = ps.register_curve_network("Order 2 contours", Viso2, Eiso2)
+    cn2.set_radius(0.002)
+    cn2.set_color((0, 0, 0))
+    cn2.set_enabled(False)
     ps.show()
