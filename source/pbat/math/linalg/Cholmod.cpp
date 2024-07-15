6bbf0141fb76f9c4bdcfc4f65c2262c40dec669f
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2024-07-15 16:41:04 -0400

    Start Cholmod integration

---
+++ source/pbat/math/linalg/Cholmod.cpp
@@ -0,0 +1,125 @@
+#include "Cholmod.h"
+
+#include <fmt/core.h>
+#include <string>
+
+namespace pbat {
+namespace math {
+namespace linalg {
+
+Cholmod::Cholmod() : mCholmodCommon{}, mCholmodA(NULL), mCholmodL(NULL), mbFactorized{false}
+{
+    cholmod_start(&mCholmodCommon);
+}
+
+void Cholmod::analyze()
+{
+    if (mCholmodL != NULL)
+        return;
+
+    mCholmodL = cholmod_analyze(mCholmodA, &mCholmodCommon);
+
+    if (mCholmodL == NULL)
+        throw std::runtime_error("Symbolic analysis of Cholesky factor failed");
+
+    mbFactorized = false;
+}
+
+bool Cholmod::factorize()
+{
+    if (mCholmodA == NULL)
+        throw std::runtime_error("Cannot factorize matrix A, which was not set.");
+
+    if (mCholmodL == NULL)
+        analyze();
+
+    int const ec = cholmod_factorize(mCholmodA, mCholmodL, &mCholmodCommon);
+    if (ec == CHOLMOD_OK)
+    {
+        mbFactorized = true;
+        return true;
+    }
+    return false;
+}
+
+MatrixX Cholmod::solve(Eigen::Ref<MatrixX const> const& B) const
+{
+    if (!mbFactorized)
+    {
+        throw std::runtime_error("Cannot solve unfactorized system.");
+    }
+
+    if (mCholmodA->ncol != static_cast<size_t>(B.rows()))
+    {
+        std::string const what = fmt::format(
+            "Expected right-hand side B to have {} rows, but got {} instead",
+            mCholmodA->ncol,
+            B.rows());
+        throw std::invalid_argument(what);
+    }
+
+    cholmod_dense cholmod_B{};
+    cholmod_B.nrow  = static_cast<size_t>(B.rows());
+    cholmod_B.ncol  = static_cast<size_t>(B.cols());
+    cholmod_B.nzmax = static_cast<size_t>(B.nonZeros());
+    cholmod_B.d     = static_cast<size_t>(B.rows());
+    cholmod_B.x     = const_cast<Scalar*>(B.data());
+    cholmod_B.xtype = CHOLMOD_REAL;
+    cholmod_B.dtype = CHOLMOD_DOUBLE;
+
+    cholmod_dense* cholmod_X = cholmod_solve(
+        CHOLMOD_A,
+        mCholmodL,
+        &cholmod_B,
+        const_cast<cholmod_common*>(&mCholmodCommon));
+
+    return Eigen::Map<MatrixX const>(
+        static_cast<Scalar*>(cholmod_X->x),
+        static_cast<Eigen::Index>(mCholmodA->nrow),
+        B.cols());
+}
+
+Cholmod::~Cholmod()
+{
+    deallocate();
+    cholmod_finish(&mCholmodCommon);
+}
+
+void Cholmod::deallocate()
+{
+    if (mCholmodA != NULL)
+    {
+        cholmod_free_sparse(&mCholmodA, &mCholmodCommon);
+    }
+    if (mCholmodL != NULL)
+    {
+        cholmod_free_factor(&mCholmodL, &mCholmodCommon);
+    }
+    mbFactorized = false;
+}
+
+} // namespace linalg
+} // namespace math
+} // namespace pbat
+
+#include <doctest/doctest.h>
+
+TEST_CASE("[math][linalg] Cholmod")
+{
+    using namespace pbat;
+    auto constexpr n = 10;
+    MatrixX const R  = MatrixX::Random(n, n);
+    CSCMatrix A      = (R.transpose() * R).sparseView();
+    A                = A.triangularView<Eigen::Lower>();
+    math::linalg::Cholmod LLT{};
+    LLT.analyze(A);
+    bool const bFactorized = LLT.factorize();
+    CHECK(bFactorized);
+    auto constexpr m        = 3;
+    MatrixX const X         = MatrixX::Random(n, m);
+    MatrixX const B         = A.selfadjointView<Eigen::Lower>() * X;
+    MatrixX const Xcomputed = LLT.solve(B);
+    Scalar const error      = (X - Xcomputed).squaredNorm();
+    auto constexpr zero     = 1e-15;
+    CHECK_LE(error, zero);
+}
---
+++ source/pbat/math/linalg/Cholmod.h
@@ -0,0 +1,110 @@
+#ifndef PBA_MATH_LINALG_CHOLMOD_H
+#define PBA_MATH_LINALG_CHOLMOD_H
+
+#include "pbat/Aliases.h"
+
+#include <algorithm>
+#include <exception>
+#include <suitesparse/cholmod.h>
+
+namespace pbat {
+namespace math {
+namespace linalg {
+
+class Cholmod
+{
+  public:
+    static_assert(
+        std::is_same_v<Scalar, double>,
+        "Cholmod implementation uses double floating point coefficients");
+    static_assert(
+        std::is_same_v<CSCMatrix::StorageIndex, std::int32_t>,
+        "Cholmod implementation uses 32-bit integer indices");
+
+    Cholmod();
+
+    template <class Derived>
+    Cholmod(Eigen::SparseCompressedBase<Derived> const& A);
+
+    template <class Derived>
+    void analyze(Eigen::SparseCompressedBase<Derived> const& A);
+
+    void analyze();
+    bool factorize();
+
+    MatrixX solve(Eigen::Ref<MatrixX const> const& B) const;
+
+    ~Cholmod();
+
+  private:
+    template <class Derived>
+    void allocate(Eigen::SparseCompressedBase<Derived> const& A);
+
+    void deallocate();
+
+    cholmod_common mCholmodCommon;
+    cholmod_sparse* mCholmodA;
+    cholmod_factor* mCholmodL;
+    bool mbFactorized;
+};
+
+template <class Derived>
+inline Cholmod::Cholmod(Eigen::SparseCompressedBase<Derived> const& A) : Cholmod()
+{
+    allocate(A);
+}
+
+template <class Derived>
+inline void Cholmod::analyze(Eigen::SparseCompressedBase<Derived> const& A)
+{
+    deallocate();
+    allocate(A);
+    analyze();
+}
+
+template <class Derived>
+inline void Cholmod::allocate(Eigen::SparseCompressedBase<Derived> const& A)
+{
+    if (A.rows() != A.cols())
+    {
+        throw std::invalid_argument("Input sparse matrix must be square");
+    }
+    if (!A.isCompressed())
+    {
+        throw std::invalid_argument("Input sparse matrix must be compressed");
+    }
+
+    // Store only the lower triangular part of A
+    mCholmodA = cholmod_allocate_sparse(
+        static_cast<size_t>(A.innerSize()),
+        static_cast<size_t>(A.outerSize()),
+        static_cast<size_t>(A.nonZeros()),
+        true /*columns are sorted*/,
+        true /*A is packed (i.e. compressed)*/,
+        -1 /*tril, i.e. lower triangular part of A is stored*/,
+        CHOLMOD_DOUBLE + CHOLMOD_REAL,
+        &mCholmodCommon);
+    if (mCholmodA == NULL)
+    {
+        throw std::runtime_error("Failed to allocate sparse matrix");
+    }
+    // cholmod_allocate_sparse probably already set this as default, but I still set it to be
+    // sure.
+    mCholmodA->itype = CHOLMOD_INT;
+
+    Scalar const* values = A.valuePtr();
+    using IndexType      = Eigen::SparseCompressedBase<Derived>::StorageIndex;
+    static_assert(std::is_same_v<IndexType, std::int32_t>, "CHOLMOD compiled with int32 indices");
+    IndexType const* innerInds = A.innerIndexPtr();
+    IndexType const* outerInds = A.outerIndexPtr();
+
+    std::memcpy(mCholmodA->p, outerInds, sizeof(IndexType) * (A.outerSize() + 1));
+    std::memcpy(mCholmodA->i, innerInds, sizeof(IndexType) * A.nonZeros());
+    std::memcpy(mCholmodA->x, values, sizeof(Scalar) * A.nonZeros());
+}
+
+} // namespace linalg
+} // namespace math
+} // namespace pbat
+
+#endif // PBA_MATH_LINALG_CHOLMOD_H
