ec73b51a32e31d4a75747a4fcea7cf8150607bf4
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2024-08-05 10:21:55 -0400

    Use Windows system search paths for DLL resolution

--- python/pbatoolkit/__init__.py
+++ python/pbatoolkit/__init__.py
@@ -1,3 +1,12 @@
+import os
+import platform
+
+# Python 3.8 and over doesn't search PATH directories for DLLs automatically https://bugs.python.org/issue36085
+if platform.system() == "Windows":
+    for dll_search_dir in os.environ["PATH"].split(";"):
+        if os.path.isdir(dll_search_dir):
+            os.add_dll_directory(dll_search_dir)
+
 import pbatoolkit.fem
 import pbatoolkit.geometry
 import pbatoolkit.gpu
