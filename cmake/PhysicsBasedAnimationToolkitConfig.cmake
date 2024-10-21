39a73874bc4e2ad57dd715e2d6d3c31016e9b5fe
Q-Minh <tonthat.quocminh@gmail.com>
2024-10-21 10:19:21 -0400

    Update CMake dependency loading

--- cmake/PhysicsBasedAnimationToolkitConfig.cmake
+++ cmake/PhysicsBasedAnimationToolkitConfig.cmake
@@ -2,29 +2,47 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(doctest)
 find_dependency(Eigen3)
-find_dependency(OpenMP)
+find_dependency(fmt)
 find_dependency(range-v3)
 find_dependency(TBB)
+find_dependency(OpenMP)
+
+unset(_pbat_find_pkg_args)
+
+if(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+    list(APPEND _pbat_find_pkg_args QUIET)
+endif()
+
+if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
+    list(APPEND _pbat_find_pkg_args REQUIRED)
+endif()
 
 if(PBAT_BUILD_PYTHON_BINDINGS)
-    if(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
-        list(APPEND _pbat_find_pkg_args QUIET)
-    endif()
-
-    if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
-        list(APPEND _pbat_find_pkg_args REQUIRED)
-    endif()
-
-    find_package(Python COMPONENTS Interpreter Development ${_pbat_find_pkg_args})
+    find_package(Python COMPONENTS Interpreter Development.Module ${_pbat_find_pkg_args})
     find_package(pybind11 CONFIG ${_pbat_find_pkg_args})
 endif()
 
 if(PBAT_USE_INTEL_MKL)
-    find_package(MKL)
+    find_dependency(MKL)
 endif()
 
 if(PBAT_USE_SUITESPARSE)
-    find_package(suitesparse)
+    find_dependency(suitesparse)
+endif()
+
+if(PBAT_USE_CUDA)
+    include(CheckLanguage)
+    check_language(CUDA)
+
+    if (NOT DEFINED CMAKE_CUDA_COMPILER)
+        if (${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
+            message(FATAL_ERROR "PBAT -- Could not find CMAKE_CUDA_COMPILER=${CMAKE_CUDA_COMPILER}")
+        endif()
+    else()
+        enable_language(CUDA)
+        find_dependency(CUDAToolkit)
+        find_dependency(cuda-api-wrappers)
+    endif()
 endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/PhysicsBasedAnimationToolkit_Targets.cmake)
