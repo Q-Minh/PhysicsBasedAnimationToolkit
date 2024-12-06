d47f760dfd4339f126b96c9b36bbbd2ef75de582
Q-Minh <tonthat.quocminh@gmail.com>
2024-12-06 12:44:29 -0500

    Add area/length test to boundary mesh

--- source/pbat/geometry/MeshBoundary.cpp
+++ source/pbat/geometry/MeshBoundary.cpp
@@ -91,20 +91,28 @@ IndexMatrixX SimplexMeshBoundary(IndexMatrixX const& C, Index n)
 } // namespace pbat
 
 #include <doctest/doctest.h>
+#include <pbat/fem/Jacobian.h>
+#include <pbat/fem/Line.h>
+#include <pbat/fem/Mesh.h>
+#include <pbat/fem/Triangle.h>
 
 TEST_CASE("[geometry] MeshBoundary")
 {
     using namespace pbat;
     // Cube mesh
+    MatrixX V(3, 8);
     IndexMatrixX C(4, 5);
     IndexMatrixX F(3, 2);
     // clang-format off
+    V << 0., 1., 0., 1., 0., 1., 0., 1.,
+        0., 0., 1., 1., 0., 0., 1., 1.,
+        0., 0., 0., 0., 1., 1., 1., 1.;
     C << 0, 3, 5, 6, 0,
         1, 2, 4, 7, 5,
         3, 0, 6, 5, 3,
         5, 6, 0, 3, 6;
-    F << 0, 1,
-        1, 0, 
+    F << 0, 2,
+        1, 1, 
         2, 3;
     // clang-format on
 
@@ -116,4 +124,20 @@ TEST_CASE("[geometry] MeshBoundary")
     CHECK_EQ(BC.cols(), 12);
     CHECK_EQ(BF.rows(), 2);
     CHECK_EQ(BF.cols(), 4);
+
+    // Check facet areas/lengths
+    using LinearTriangle = fem::Triangle<1>;
+    using TriangleMesh   = fem::Mesh<LinearTriangle, 3>;
+    TriangleMesh FM(V, BC);
+    VectorX FA = fem::InnerProductWeights<1>(FM).reshaped();
+    bool const bTrianglesHaveCorrectAreas =
+        (FA.array() - Scalar(0.5)).square().sum() < Scalar(1e-10);
+    CHECK(bTrianglesHaveCorrectAreas);
+    using LinearLine = fem::Line<1>;
+    using LineMesh   = fem::Mesh<LinearLine, 3>;
+    LineMesh EM(V, BF);
+    VectorX EL = fem::InnerProductWeights<1>(EM).reshaped();
+    bool const bLineSegmentsHaveCorrectLengths =
+        (EL.array() - Scalar(1)).square().sum() < Scalar(1e-10);
+    CHECK(bLineSegmentsHaveCorrectLengths);
 }
