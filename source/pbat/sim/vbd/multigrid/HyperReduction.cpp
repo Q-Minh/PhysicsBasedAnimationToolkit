4d7cda3e9716fb23cd2878d0627968317c867ed0
Q-Minh <tonthat.quocminh@gmail.com>
2025-01-14 00:05:15 -0500

    Implement representative element selection in clusters for HyperReduction

--- source/pbat/sim/vbd/multigrid/HyperReduction.cpp
+++ source/pbat/sim/vbd/multigrid/HyperReduction.cpp
@@ -1,6 +1,7 @@
 #include "HyperReduction.h"
 
 #include "pbat/common/Indexing.h"
+#include "pbat/geometry/TetrahedralAabbHierarchy.h"
 #include "pbat/graph/Adjacency.h"
 #include "pbat/graph/Mesh.h"
 #include "pbat/graph/Partition.h"
@@ -21,30 +22,46 @@ namespace multigrid {
 HyperReduction::HyperReduction(Hierarchy const& hierarchy, Index clusterSize)
     : Cptr(hierarchy.levels.size()),
       Cadj(hierarchy.levels.size()),
+      eC(hierarchy.levels.size()),
+      PinvC(hierarchy.levels.size()),
       Ep(hierarchy.levels.size() + 1),
-      EpMax(0)
+      EpMax(1e-6)
 {
     PBAT_PROFILE_NAMED_SCOPE("pbat.sim.vbd.multigrid.HyperReduction.Construct");
+    ConstructHierarchicalClustering(hierarchy, clusterSize);
+    SelectClusterRepresentatives(hierarchy);
+    PrecomputeInversePolynomialMatrices(hierarchy);
+}
 
+void HyperReduction::ConstructHierarchicalClustering(Hierarchy const& hierarchy, Index clusterSize)
+{
+    PBAT_PROFILE_NAMED_SCOPE(
+        "pbat.sim.vbd.multigrid.HyperReduction.ConstructHierarchicalClustering");
+
+    // Construct the graph of face-adjacent fine mesh elements
     IndexVectorX Gptr, Gadj, Gwts;
     std::tie(Gptr, Gadj, Gwts) = graph::MatrixToWeightedAdjacency(graph::MeshDualGraph(
         hierarchy.data.E,
         hierarchy.data.X.cols(),
         graph::EMeshDualGraphOptions::FaceAdjacent));
-    std::size_t nLevels        = hierarchy.levels.size();
-    for (std::size_t l = 0; l < nLevels; l++)
+
+    // Ask for contiguous partitions
+    graph::PartitioningOptions opts{};
+    opts.eCoarseningStrategy =
+        graph::PartitioningOptions::ECoarseningStrategy::SortedHeavyEdgeMatching;
+    opts.rngSeed                        = 0;
+    opts.bMinimizeSupernodalGraphDegree = true;
+    opts.bEnforceContiguousPartitions   = true;
+    opts.bIdentifyConnectedComponents   = true;
+
+    // Construct the clustering at each level
+    auto nLevels = hierarchy.levels.size();
+    for (decltype(nLevels) l = 0; l < nLevels; l++)
     {
         // Reduce the graph via partitioning/clustering
         auto nGraphNodes = Gptr.size() - 1;
         auto nPartitions = nGraphNodes / clusterSize;
-        graph::PartitioningOptions opts{};
-        opts.eCoarseningStrategy =
-            graph::PartitioningOptions::ECoarseningStrategy::SortedHeavyEdgeMatching;
-        opts.rngSeed                        = 0;
-        opts.bMinimizeSupernodalGraphDegree = true;
-        opts.bEnforceContiguousPartitions   = true;
-        opts.bIdentifyConnectedComponents   = true;
-        auto clustering                     = graph::Partition(Gptr, Gadj, Gwts, nPartitions, opts);
+        auto clustering  = graph::Partition(Gptr, Gadj, Gwts, nPartitions, opts);
         // Store the l^{th} level clustering
         std::tie(Cptr[l], Cadj[l]) = graph::MapToAdjacency(clustering);
         // Compute the supernodal graph (i.e. graph of clusters) as next graph to partition
@@ -66,6 +83,55 @@ HyperReduction::HyperReduction(Hierarchy const& hierarchy, Index clusterSize)
         std::vector<graph::WeightedEdge<Index, Index>> edges(edgeView.begin(), edgeView.end());
         std::tie(Gptr, Gadj, Gwts) = graph::MatrixToWeightedAdjacency(
             graph::AdjacencyMatrixFromEdges(edges.begin(), edges.end()));
+    }
+}
+
+void HyperReduction::SelectClusterRepresentatives(Hierarchy const& hierarchy)
+{
+    PBAT_PROFILE_NAMED_SCOPE("pbat.sim.vbd.multigrid.HyperReduction.SelectClusterRepresentatives");
+
+    auto const& data     = hierarchy.data;
+    auto const& E        = data.E;
+    auto const& X        = data.X;
+    auto const nElements = E.cols();
+    auto const dims      = X.rows();
+    MatrixX centroids(dims, nElements);
+
+    // Compute element centroids
+    tbb::parallel_for(Index(0), nElements, [&](Index e) {
+        auto const& x    = X(E.col(e));
+        centroids.col(e) = x.rowwise().mean();
+    });
+
+    // Select each level's cluster representatives
+    auto nLevels = hierarchy.levels.size();
+    for (decltype(nLevels) l = 0; l < nLevels; l++)
+    {
+        auto const& cptr     = Cptr[l];
+        auto const& cadj     = Cadj[l];
+        auto& ec             = eC[l];
+        auto const nClusters = cptr.size() - 1;
+        tbb::parallel_for(Index(0), nClusters, [&](Index c) {
+            auto const& cluster = cadj(Eigen::seqN(cptr(c), cptr(c + 1)));
+            auto const& Xc      = centroids(Eigen::placeholders::all, cluster);
+            auto const muXc     = Xc.rowwise().mean().eval();
+            auto const d2mu     = (Xc.array().colwise() - muXc.array()).colwise().squaredNorm();
+            Index eMu;
+            Scalar d2eMu     = d2mu.minCoeff(&eMu);
+            ec(c)            = cluster(eMu);
+            centroids.col(c) = muXc;
+        });
+    }
+}
+
+void HyperReduction::PrecomputeInversePolynomialMatrices(Hierarchy const& hierarchy)
+{
+    PBAT_PROFILE_NAMED_SCOPE(
+        "pbat.sim.vbd.multigrid.HyperReduction.PrecomputeInversePolynomialMatrices");
+
+    auto nLevels = hierarchy.levels.size();
+    for (decltype(nLevels) l = 0; l < nLevels; l++)
+    {
     }
 }
 
--- source/pbat/sim/vbd/multigrid/HyperReduction.h
+++ source/pbat/sim/vbd/multigrid/HyperReduction.h
@@ -15,6 +15,37 @@ struct Hierarchy;
 struct HyperReduction
 {
     HyperReduction(Hierarchy const& hierarchy, Index clusterSize = 5);
+    /**
+     * @brief Construct the hierarchical clustering of mesh elements
+     *
+     * @param hierarchy
+     * @param clusterSize
+     */
+    void ConstructHierarchicalClustering(Hierarchy const& hierarchy, Index clusterSize);
+    /**
+     * @brief
+     *
+     * @param hierarchy
+     */
+    void SelectClusterRepresentatives(Hierarchy const& hierarchy);
+    /**
+     * @brief
+     *
+     * @param hierarchy
+     */
+    void PrecomputeInversePolynomialMatrices(Hierarchy const& hierarchy);
+    /**
+     * @brief Set the maximum allowable polynomial error threshold
+     *
+     * @param value
+     */
+    void SetMaximumAllowablePolynomialError(Scalar value) { EpMax = value; }
+    /**
+     * @brief Get the maximum allowable polynomial error
+     *
+     * @return Scalar
+     */
+    Scalar GetMaximumAllowablePolynomialError() const { return EpMax; }
 
     /**
      * @brief Hierarchical clustering of mesh elements
@@ -22,6 +53,12 @@ struct HyperReduction
     std::vector<IndexVectorX> Cptr;
     std::vector<IndexVectorX> Cadj;
 
+    std::vector<IndexVectorX> eC; ///< |#levels| list of |#clusters| arrays of representative
+                                   ///< elements in each cluster of the corresponding level
+    std::vector<MatrixX>
+        PinvC; ///< |#levels| list of 4x|4*#clusters| of A_p^{-1} matrices, such that A_p's
+               ///< coefficients are \int_{\Omega^c} P_i(X) P_j(X) dx, where \Omega^c is cluster c's
+               ///< domain, and P_k(X) is the k^{th} polynomial basis.
     std::vector<VectorX>
         Ep;       ///< |#levels+1| linear polynomial errors at fine elements and at each level
     Scalar EpMax; ///< Maximum allowable linear polynomial error in any cluster
