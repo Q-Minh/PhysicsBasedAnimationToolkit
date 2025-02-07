6a98560cc641d71948812856caac4b00afe04be5
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2025-02-07 16:48:27 -0500

    Fix Queue initialization bug

--- source/pbat/common/Queue.h
+++ source/pbat/common/Queue.h
@@ -10,23 +10,24 @@ template <class T, auto kCapacity = 64>
 class Queue
 {
   public:
-    PBAT_HOST_DEVICE Queue() : queue{}, begin{0}, end{0} {}
+    PBAT_HOST_DEVICE Queue() : queue{}, begin{0}, end{0}, n{0} {}
     PBAT_HOST_DEVICE void Push(T value)
     {
         queue[end] = value;
-        end        = (end + 1) % kCapacity;
+        end        = (end + 1) % Capacity();
         ++n;
     }
     PBAT_HOST_DEVICE T const& Top() const { return queue[begin]; }
     PBAT_HOST_DEVICE void Pop()
     {
-        begin = (begin + 1) % kCapacity;
+        begin = (begin + 1) % Capacity();
         --n;
     }
-    PBAT_HOST_DEVICE bool IsFull() const { return n == kCapacity; }
+    PBAT_HOST_DEVICE bool IsFull() const { return n == Capacity(); }
     PBAT_HOST_DEVICE bool IsEmpty() const { return n == 0; }
     PBAT_HOST_DEVICE auto Size() const { return n; }
     PBAT_HOST_DEVICE void Clear() { begin = end = n = 0; }
+    PBAT_HOST_DEVICE constexpr auto Capacity() const { return kCapacity; }
 
   private:
     T queue[kCapacity];
