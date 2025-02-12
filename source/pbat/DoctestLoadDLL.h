d2869b83f57d565aee492bafc1d849849a104726
Q-Minh <tonthat.quocminh@gmail.com>
2025-02-11 20:05:57 -0500

    Document DLL loading dummy function

--- source/pbat/DoctestLoadDLL.h
+++ source/pbat/DoctestLoadDLL.h
@@ -1,3 +1,13 @@
+/**
+ * @file DoctestLoadDLL.h
+ * @author Quoc-Minh Ton-That (tonthat.quocminh@gmail.com)
+ * @brief No-op function to force load PhysicsBasedAnimationToolkit DLL
+ * @date 2025-02-11
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
 #ifndef PBAT_DOCTEST_LOAD_DLL_H
 #define PBAT_DOCTEST_LOAD_DLL_H
 
@@ -5,8 +15,11 @@
 
 namespace pbat {
 
+/**
+ * @brief No-op function to force load PhysicsBasedAnimationToolkit DLL
+ */
 PBAT_API void ForceLoadDLL();
 
-} // namespace pba
+} // namespace pbat
 
 #endif // PBAT_DOCTEST_LOAD_DLL_H
