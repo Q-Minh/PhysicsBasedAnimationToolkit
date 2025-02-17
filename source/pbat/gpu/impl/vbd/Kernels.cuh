e0cf4da36b79c62385708710ef869a031cacf328
Q-Minh <tonthat.quocminh@gmail.com>
2025-02-16 23:04:06 -0500

    Improve kernel memory usage

--- source/pbat/gpu/impl/vbd/Kernels.cuh
+++ source/pbat/gpu/impl/vbd/Kernels.cuh
@@ -131,9 +131,9 @@ __global__ void VbdIteration(BackwardEulerMinimization BDF)
     GpuIndex nAdjacentElements = BDF.GVTp[i + 1] - GVTbegin;
     // 1. Compute vertex-element elastic energy derivatives w.r.t. i and store them in shared
     // memory
-    SMatrix<GpuScalar, 3, 4> Hgt = Zeros<GpuScalar, 3, 4>();
-    auto Ht                      = Hgt.Slice<3, 3>(0, 0);
-    auto gt                      = Hgt.Col(3);
+    SMatrix<GpuScalar, 3, 4> Hgi = Zeros<GpuScalar, 3, 4>();
+    auto Hi                      = Hgi.Slice<3, 3>(0, 0);
+    auto gi                      = Hgi.Col(3);
     for (auto elocal = tid; elocal < nAdjacentElements; elocal += kBlockThreads)
     {
         GpuIndex e                   = BDF.GVTn[GVTbegin + elocal];
@@ -150,21 +150,22 @@ __global__ void VbdIteration(BackwardEulerMinimization BDF)
         Psi.gradAndHessian(Fe, lamee(0), lamee(1), gF, HF);
         using pbat::sim::vbd::kernels::AccumulateElasticGradient;
         using pbat::sim::vbd::kernels::AccumulateElasticHessian;
-        AccumulateElasticHessian(ilocal, wg, GPe, HF, Ht);
-        AccumulateElasticGradient(ilocal, wg, GPe, gF, gt);
+        AccumulateElasticHessian(ilocal, wg, GPe, HF, Hi);
+        AccumulateElasticGradient(ilocal, wg, GPe, gF, gi);
     }
 
     // 2. Compute total vertex hessian and gradient via parallel reduction
-    SMatrix<GpuScalar, 3, 4> Hgi = BlockReduce(reinterpret_cast<BlockStorage&>(shared)).Sum(Hgt);
+    Hgi = BlockReduce(reinterpret_cast<BlockStorage&>(shared)).Sum(Hgi);
     if (tid > 0)
         return;
-    auto Hi = Hgi.Slice<3, 3>(0, 0);
-    auto gi = Hgi.Col(3);
+
+    // Load vertex data
+    GpuScalar mi                  = BDF.m[i];
+    SVector<GpuScalar, 3> xti     = FromBuffers<3, 1>(BDF.xt, i);
+    SVector<GpuScalar, 3> xitilde = FromBuffers<3, 1>(BDF.xtilde, i);
+    SVector<GpuScalar, 3> xi      = FromBuffers<3, 1>(BDF.x, i);
 
     // 3. Add stiffness damping
-    GpuScalar mi              = BDF.m[i];
-    SVector<GpuScalar, 3> xti = FromBuffers<3, 1>(BDF.xt, i);
-    SVector<GpuScalar, 3> xi  = FromBuffers<3, 1>(BDF.x, i);
     using pbat::sim::vbd::kernels::AddDamping;
     AddDamping(BDF.dt, xti, xi, BDF.kD, gi, Hi);
 
@@ -197,7 +198,6 @@ __global__ void VbdIteration(BackwardEulerMinimization BDF)
     }
 
     // 4. Add inertial term
-    SVector<GpuScalar, 3> xitilde = FromBuffers<3, 1>(BDF.xtilde, i);
     using pbat::sim::vbd::kernels::AddInertiaDerivatives;
     AddInertiaDerivatives(BDF.dt2, mi, xitilde, xi, gi, Hi);
 
@@ -266,9 +266,10 @@ __global__ static void AccumulateVertexEnergies(BackwardEulerMinimization BDF)
         return;
 
     // Load vertex data
-    GpuScalar mi              = BDF.m[i];
-    SVector<GpuScalar, 3> xti = FromBuffers<3, 1>(BDF.xt, i);
-    SVector<GpuScalar, 3> xi  = FromBuffers<3, 1>(BDF.x, i);
+    GpuScalar mi                  = BDF.m[i];
+    SVector<GpuScalar, 3> xti     = FromBuffers<3, 1>(BDF.xt, i);
+    SVector<GpuScalar, 3> xitilde = FromBuffers<3, 1>(BDF.xtilde, i);
+    SVector<GpuScalar, 3> xi      = FromBuffers<3, 1>(BDF.x, i);
 
     // 2. Add contact energy
     static auto constexpr kMaxContacts = BackwardEulerMinimization::kMaxCollidingTrianglesPerVertex;
@@ -299,7 +300,6 @@ __global__ static void AccumulateVertexEnergies(BackwardEulerMinimization BDF)
     }
 
     // 4. Add inertial term
-    SVector<GpuScalar, 3> xitilde = FromBuffers<3, 1>(BDF.xtilde, i);
     fi += GpuScalar(0.5) * mi * SquaredNorm(xi - xti);
 
     // 5. Store vertex objective function
