1a425e85723cbc7854de230605911aee000c469c
Q-Minh <tonthat.quocminh@gmail.com>
2025-02-18 22:01:08 -0500

    Add Python documentation to vbd Data

--- bindings/pypbat/sim/vbd/Data.cpp
+++ bindings/pypbat/sim/vbd/Data.cpp
@@ -36,31 +36,51 @@ void BindData(pybind11::module& m)
             pyb::arg("X"),
             pyb::arg("T"),
             "Sets the FEM simulation mesh as array of 3x|#nodes| positions X and 4x|#elements| "
-            "tetrahedral elements T.")
+            "tetrahedral elements T.\n\n"
+            "Args:\n"
+            "    X (numpy.ndarray): 3x|#nodes| array of vertex positions\n"
+            "    T (numpy.ndarray): 4x|#elements| array of tetrahedral elements\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_surface_mesh",
             &Data::WithSurfaceMesh,
             pyb::arg("V"),
             pyb::arg("F"),
             "Sets the collision mesh as array of 1x|#collision vertices| indices V into positions "
-            "X and 3x|#collision triangles| indices into X.")
+            "X and 3x|#collision triangles| indices into X.\n\n"
+            "Args:\n"
+            "    V (numpy.ndarray): 1x|#collision vertices| array of vertex indices\n"
+            "    F (numpy.ndarray): 3x|#collision triangles| array of triangle indices\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_bodies",
             &Data::WithBodies,
             pyb::arg("B"),
             "Sets the body indices of each vertex.\n\n"
             "Args:\n"
-            "    B (numpy.ndarray): 1x|#nodes| array of body indices.")
+            "    B (numpy.ndarray): 1x|#nodes| array of body indices.\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_velocity",
             &Data::WithVelocity,
             pyb::arg("v"),
-            "Sets the 3x|#nodes| initial velocity field at FEM nodes.")
+            "Sets the 3x|#nodes| initial velocity field at FEM nodes.\n\n"
+            "Args:\n"
+            "    v (numpy.ndarray): 3x|#nodes| array of initial velocities.\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_acceleration",
             &Data::WithAcceleration,
             pyb::arg("a"),
-            "Sets the 3x|#nodes| external acceleration field at FEM nodes.")
+            "Sets the 3x|#nodes| external acceleration field at FEM nodes.\n\n"
+            "Args:\n"
+            "    a (numpy.ndarray): 3x|#nodes| array of external accelerations.\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_material",
             &Data::WithMaterial,
@@ -68,25 +88,55 @@ void BindData(pybind11::module& m)
             pyb::arg("mue"),
             pyb::arg("lambdae"),
             "Sets the |#elements| array of mass densities, |#elements| array of 1st Lame "
-            "coefficients and |#elements| array of 2nd Lame coefficients.")
+            "coefficients and |#elements| array of 2nd Lame coefficients.\n\n"
+            "Args:\n"
+            "    rhoe (numpy.ndarray): |#elements| array of mass densities\n"
+            "    mue (numpy.ndarray): |#elements| array of 1st Lame coefficients\n"
+            "    lambdae (numpy.ndarray): |#elements| array of 2nd Lame coefficients\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_dirichlet_vertices",
             &Data::WithDirichletConstrainedVertices,
             pyb::arg("dbc"),
             pyb::arg("muD")          = Scalar(1),
             pyb::arg("input_sorted") = true,
-            "Sets Dirichlet constrained vertices.")
+            "Sets Dirichlet constrained vertices.\n\n"
+            "Args:\n"
+            "    dbc (numpy.ndarray): Dirichlet constrained vertices\n"
+            "    muD (float): Dirichlet penalty\n"
+            "    input_sorted (bool): True if dbc is sorted\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_vertex_coloring_strategy",
             &Data::WithVertexColoringStrategy,
             pyb::arg("ordering"),
             pyb::arg("selection"),
-            "Sets the vertex coloring strategy to use.")
+            "Sets the vertex coloring strategy to use.\n\n"
+            "Args:\n"
+            "    ordering (int): Vertex visit order\n"
+            "    selection (int): Color selection strategy\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_initialization_strategy",
             &Data::WithInitializationStrategy,
-            pyb::arg("strategy"))
-        .def("with_rayleigh_damping", &Data::WithRayleighDamping, pyb::arg("kD"))
+            pyb::arg("strategy"),
+            "Sets the non-linear optimization initialization strategy.\n\n"
+            "Args:\n"
+            "    strategy (int): Initialization strategy\n\n"
+            "Returns:\n"
+            "    Data: self")
+        .def(
+            "with_rayleigh_damping",
+            &Data::WithRayleighDamping,
+            pyb::arg("kD"),
+            "Sets the Rayleigh damping coefficient.\n\n"
+            "Args:\n"
+            "    kD (float): Damping coefficient\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_contact_parameters",
             &Data::WithContactParameters,
@@ -98,24 +148,37 @@ void BindData(pybind11::module& m)
             "    muC (float): Collision penalty\n"
             "    muF (float): Friction coefficient\n"
             "    epsv (float): IPC's relative velocity threshold for static to dynamic friction's "
-            "smooth transition.")
+            "smooth transition.\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_active_set_update_frequency",
             &Data::WithActiveSetUpdateFrequency,
             pyb::arg("frequency"),
             "Sets the contact constraint active set update frequency in a given time step (i.e. "
-            "update vertex-triangle contact pairs every 'frequency' substeps).")
+            "update vertex-triangle contact pairs every 'frequency' substeps).\n\n"
+            "Args:\n"
+            "    frequency (int): Active set update frequency\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_hessian_determinant_zero",
             &Data::WithHessianDeterminantZeroUnder,
-            pyb::arg("zero"))
+            pyb::arg("zero"),
+            "Sets the numerical zero used in 'singular' hessian determinant check.\n\n"
+            "Args:\n"
+            "    zero (float): Numerical zero\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_chebyshev_acceleration",
             &Data::WithChebyshevAcceleration,
             pyb::arg("rho"),
             "Use Chebyshev semi-iterative method's\n\n"
             "Args:\n"
-            "    rho (float): Estimated spectral radius. rho must be in (0, 1).")
+            "    rho (float): Estimated spectral radius. rho must be in (0, 1).\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def(
             "with_trust_region_acceleration",
             &Data::WithTrustRegionAcceleration,
@@ -126,8 +189,18 @@ void BindData(pybind11::module& m)
             "Args:\n"
             "    eta (float): Energy reduction accuracy threshold\n"
             "    tau (float): Trust Region radius increase factor\n"
-            "    curved (bool): Use curved accelerated path, otherwise use linear path.")
-        .def("construct", &Data::Construct, pyb::arg("validate") = true)
+            "    curved (bool): Use curved accelerated path, otherwise use linear path.\n\n"
+            "Returns:\n"
+            "    Data: self")
+        .def(
+            "construct",
+            &Data::Construct,
+            pyb::arg("validate") = true,
+            "Constructs the VBD data.\n\n"
+            "Args:\n"
+            "    validate (bool): True to validate the data\n\n"
+            "Returns:\n"
+            "    Data: self")
         .def_readwrite("X", &Data::X, "3x|#nodes| array of vertex positions")
         .def_readwrite("E", &Data::E, "4x|#elements| array of tetrahedral elements")
         .def_readwrite("V", &Data::V, "1x|#collision vertices| array of vertex indices")
