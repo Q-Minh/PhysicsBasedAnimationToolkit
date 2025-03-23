0570f03dc8d5f00aa1597f45b2143910167dfcbb
Q-Minh <tonthat.quocminh@gmail.com>
2025-03-23 11:29:56 -0400

    Document Heap

--- source/pbat/common/Heap.h
+++ source/pbat/common/Heap.h
@@ -17,31 +17,68 @@
 
 namespace pbat::common {
 
+/**
+ * @brief Fixed-size max heap
+ *
+ * @tparam T Type of the elements
+ * @tparam Less Less-than comparator
+ * @tparam kCapacity Capacity of the heap
+ */
 template <class T, class Less = std::less<T>, auto kCapacity = 64>
 class Heap
 {
   public:
+    /**
+     * @brief Construct a new Heap object
+     *
+     * @param less Comparator for the heap
+     */
     PBAT_HOST_DEVICE Heap(Less less = Less{}) : heap{}, less(less), size{0} {}
+    /**
+     * @brief Push an element to the heap
+     *
+     * @param value Element to push
+     */
     PBAT_HOST_DEVICE void Push(T value)
     {
         assert(size < kCapacity);
         heap[size++] = value;
         std::push_heap(heap, heap + size, less);
     }
+    /**
+     * @brief Pop the top element from the heap
+     *
+     * @return Top element
+     */
     PBAT_HOST_DEVICE T Pop()
     {
         assert(not IsEmpty());
         std::pop_heap(heap, heap + size--, less);
         return heap[size];
     }
+    /**
+     * @brief Get the top element of the heap
+     *
+     * @return Top element
+     */
     PBAT_HOST_DEVICE T const& Top() const { return heap[0]; }
+    /**
+     * @brief Get the size of the heap
+     *
+     * @return Size of the heap
+     */
     PBAT_HOST_DEVICE auto Size() const { return size; }
+    /**
+     * @brief Check if the heap is empty
+     *
+     * @return true if the heap is empty, false otherwise
+     */
     PBAT_HOST_DEVICE bool IsEmpty() const { return size == 0; }
 
   private:
-    T heap[kCapacity];
-    Less less;
-    int size;
+    T heap[kCapacity]; ///< Buffer allocated for storing elements
+    Less less;         ///< Comparator
+    int size;          ///< Size of the heap
 };
 
 } // namespace pbat::common
