3914ab2821442a0c43e6ee6baffbc78c655677de
Q-Minh <tonthat.quocminh@gmail.com>
2025-03-30 17:01:39 -0400

    Add sign compare warning

--- source/pbat/warning/SignConversion.h
+++ source/pbat/warning/SignConversion.h
@@ -1,7 +1,7 @@
 #if defined(__clang__)
-    #pragma clang diagnostic ignored "-Wconversion"
+    #pragma clang diagnostic ignored "-Wsign-conversion"
 #elif defined(__GNUC__) || defined(__GNUG__)
-    #pragma GCC diagnostic ignored "-Wconversion"
+    #pragma GCC diagnostic ignored "-Wsign-conversion"
 #elif defined(_MSC_VER)
     #pragma warning(disable : 4365)
 #endif
