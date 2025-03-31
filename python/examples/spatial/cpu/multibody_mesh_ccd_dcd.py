851e397550c1f490438f0f4d7c75cf7b7d1a4eb2
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2025-03-31 16:05:40 -0400

    Implement multibody system construction in CCD and DCD example

--- python/examples/spatial/cpu/multibody_mesh_ccd_dcd.py
+++ python/examples/spatial/cpu/multibody_mesh_ccd_dcd.py
@@ -4,6 +4,53 @@ import meshio
 import numpy as np
 import polyscope as ps
 import polyscope.imgui as imgui
+import igl
+
+
+def faces(TS):
+    FS = [igl.boundary_facets(T) for T in TS]
+    for i, F in enumerate(FS):
+        F[:, :2] = np.roll(F[:, :2], shift=1, axis=1)
+    return FS
+
+
+def edges(FS):
+    ES = [igl.edges(F) for F in FS]
+    return ES
+
+
+def vertices(FS):
+    VS = [np.unique(F) for F in FS]
+    return VS
+
+
+def prefix_sum(counts):
+    p = np.zeros(len(counts) + 1, dtype=np.int64)
+    p[1:] = np.cumsum(counts)
+    return p
+
+
+def build_multibody_system(VS, TS):
+    Xlen = [X.shape[0] for X in VS]
+    XP = prefix_sum(Xlen)
+    X = np.hstack([V.T for V in VS])
+    FS = faces(TS)
+    ES = edges(FS)
+    VS = vertices(FS)
+    Tlen = [T.shape[0] for T in TS]
+    Flen = [F.shape[0] for F in FS]
+    Elen = [E.shape[0] for E in ES]
+    Vlen = [V.shape[0] for V in VS]
+    TP = prefix_sum(Tlen)
+    FP = prefix_sum(Flen)
+    EP = prefix_sum(Elen)
+    VP = prefix_sum(Vlen)
+    T = np.hstack([T.T + XP[k] for k, T in enumerate(TS)])
+    F = np.hstack([F.T + XP[k] for k, F in enumerate(FS)])
+    E = np.hstack([E.T + XP[k] for k, E in enumerate(ES)])
+    V = np.hstack([V.T + XP[k] for k, V in enumerate(VS)])
+    return X, V, E, F, T, VP, EP, FP, TP
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -55,7 +102,7 @@ if __name__ == "__main__":
     aabb = pbat.geometry.aabb(V.T)
     extents = aabb.max - aabb.min
     separation = args.separation * extents
-    meshes = [
+    VS = [
         V
         + np.array(
             [
@@ -68,6 +115,9 @@ if __name__ == "__main__":
         for j in range(gcols)
         for i in range(grows)
     ]
+    TS = [T for _ in range(gheight) for _ in range(gcols) for _ in range(grows)]
+    X, V, E, F, T, VP, EP, FP, TP = build_multibody_system(VS, TS)
+    U = np.zeros_like(X)
 
     ps.init()
     ps.set_verbosity(0)
@@ -77,18 +127,21 @@ if __name__ == "__main__":
     ps.set_ground_plane_height_factor(0.5)
     ps.set_program_name("3D multibody mesh CCD and DCD contact detection algorithm")
 
-    vm = [ps.register_volume_mesh(f"Mesh {m}", VI, T) for m, VI in enumerate(meshes)]
+    sm = ps.register_surface_mesh("Surface mesh", X.T, F.T)
+
     t = 0
     def callback():
         global t
-        for m, VI in enumerate(meshes):
+        U[:] = 0
+        for m in range(VP.shape[0] - 1):
             r = m % 3
             # Update mesh position
             offset = args.separation * extents
             dir = np.ones(3)
             dir[r] = -1
             u = args.amplitude * dir * offset * np.sin(t * args.frequency)
-            vm[m].update_vertex_positions(VI + u)
+            U[:, V[VP[m]:VP[m + 1]]] = u[:, np.newaxis]
+        sm.update_vertex_positions((X+U).T)
         t = t + 1 / 60
 
     ps.set_user_callback(callback)
