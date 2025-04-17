d8f7b99c0018e5558e60a0d83b97ff4769d996b8
Q-Minh <tonthat.quocminh@gmail.com>
2025-04-17 15:07:46 -0400

    Test cuBLAS submatrix API

--- source/pbat/gpu/impl/math/Matrix.cu
+++ source/pbat/gpu/impl/math/Matrix.cu
@@ -23,7 +23,7 @@ Blas::~Blas()
 
 #include <doctest/doctest.h>
 
-TEST_CASE("[gpu][impl][math] Blas")
+TEST_CASE("[gpu][impl][math] Matrix")
 {
     using namespace pbat;
     using pbat::gpu::impl::common::Buffer;
@@ -31,24 +31,43 @@ TEST_CASE("[gpu][impl][math] Blas")
     using pbat::gpu::impl::common::ToEigen;
     using pbat::gpu::impl::math::Blas;
     using namespace pbat::gpu::impl;
-
-    auto constexpr m = 10;
-    auto constexpr n = 5;
-    GpuMatrixX A     = GpuMatrixX::Random(m, n);
-    GpuVectorX x     = GpuVectorX::Random(n);
-    GpuVectorX y     = A * x;
+    // Arrange
+    auto constexpr eps = 1e-6f;
+    auto constexpr m   = 10;
+    auto constexpr n   = 5;
+    GpuMatrixX A       = GpuMatrixX::Random(m, n);
+    GpuVectorX x       = GpuVectorX::Random(n);
+    GpuVectorX y       = A * x;
     math::Matrix<GpuScalar> dA(m, n);
     math::Vector<GpuScalar> dx(n);
     math::Vector<GpuScalar> dy(m);
     ToBuffer(A, dA.data);
     ToBuffer(x, dx.data);
     ToBuffer(y, dy.data);
-
     Blas blas;
-    blas.Gemv(dA, dx, dy);
 
-    auto yEigen          = ToEigen(dy.data);
-    auto constexpr eps   = 1e-6f;
-    bool const bAreEqual = y.isApprox(yEigen.reshaped(), eps);
-    CHECK(bAreEqual);
+    SUBCASE("Full matrices and vectors")
+    {
+        // Act
+        blas.Gemv(dA, dx, dy);
+        // Assert
+        auto yEigen          = ToEigen(dy.data);
+        bool const bAreEqual = y.isApprox(yEigen.reshaped(), eps);
+        CHECK(bAreEqual);
+    }
+    SUBCASE("Sub-matrices and sub-vectors")
+    {
+        // Arrange
+        auto dAS              = dA.SubMatrix(1, 1, 3, 3);
+        auto dxS              = dx.Segment(1, 3);
+        auto dyS              = dy.Segment(1, 3);
+        GpuVectorX ySExpected = A.block(1, 1, 3, 3) * x.segment(1, 3) + y.segment(1, 3);
+        // Act
+        blas.Gemv(dAS, dxS, dyS, 1.f, 1.f);
+        // Assert
+        auto yEigen          = ToEigen(dy.data);
+        auto ySEigen         = yEigen.reshaped().segment(1, 3);
+        bool const bAreEqual = ySEigen.isApprox(ySExpected, eps);
+        CHECK(bAreEqual);
+    }
 }
--- source/pbat/gpu/impl/math/Matrix.cuh
+++ source/pbat/gpu/impl/math/Matrix.cuh
@@ -8,6 +8,7 @@
 #include <cublas_v2.h>
 #include <cuda/api/stream.hpp>
 #include <exception>
+#include <type_traits>
 
 #define CUBLAS_CHECK(err)                                                        \
     {                                                                            \
@@ -46,128 +47,154 @@ concept CVector = requires(TVector a)
 template <class T>
 struct VectorView
 {
-    using ValueType = T;
+    using ValueType = std::remove_cvref_t<T>;
 
-    T* data; ///< Pointer to the vector coefficients
-    int n;   ///< Number of rows
-    int inc; ///< Increment
+    ValueType* data; ///< Pointer to the vector coefficients
+    int n;           ///< Number of rows
+    int inc;         ///< Increment
 
     // Storage information
-    T* Raw() { return data; }
-    T const* Raw() const { return data; }
+    ValueType* Raw() { return data; }
+    ValueType const* Raw() const { return data; }
     auto Rows() const { return n; }
     constexpr auto Cols() const { return 1; }
     auto Increment() const { return inc; }
 
     // Accessors
-    VectorView<T> Slice(auto row, auto rows, auto inc) const
+    VectorView<ValueType> Slice(auto row, auto rows, auto inc) const
     {
-        return VectorView<T>{data + row, rows, inc};
+        return VectorView<ValueType>{const_cast<ValueType*>(data) + row, rows, inc};
     }
-    VectorView<T> Segment(auto row, auto rows) const { return Slice(row, rows, 1); }
-    VectorView<T> Head(auto rows) const { return Slice(0, rows, 1); }
-    VectorView<T> Tail(auto rows) const { return Slice(n - rows, rows, 1); }
+    VectorView<ValueType> Segment(auto row, auto rows) const { return Slice(row, rows, 1); }
+    VectorView<ValueType> Head(auto rows) const { return Slice(0, rows, 1); }
+    VectorView<ValueType> Tail(auto rows) const { return Slice(n - rows, rows, 1); }
 };
 
 template <class T>
 struct MatrixView
 {
-    using ValueType = T;
+    using ValueType = std::remove_cvref_t<T>;
 
-    T* data;              ///< Pointer to the matrix coefficients
+    ValueType* data;      ///< Pointer to the matrix coefficients
     int m;                ///< Number of rows
     int n;                ///< Number of columns
     int ld;               ///< Leading dimension
     cublasOperation_t op; ///< CUBLAS operation type
 
     // Storage information
-    T* Raw() { return data; }
-    T const* Raw() const { return data; }
+    ValueType* Raw() { return data; }
+    ValueType const* Raw() const { return data; }
     auto Rows() const { return m; }
     auto Cols() const { return n; }
     auto LeadingDimensions() const { return ld; }
     auto Operation() const { return op; }
 
     // Accessors
-    MatrixView<T> Transposed() const
+    MatrixView<ValueType> Transposed() const
     {
-        return MatrixView<T>{data, m, n, ld, op == CUBLAS_OP_N ? CUBLAS_OP_T : CUBLAS_OP_N};
+        return MatrixView<ValueType>{
+            const_cast<ValueType*>(data),
+            m,
+            n,
+            ld,
+            op == CUBLAS_OP_N ? CUBLAS_OP_T : CUBLAS_OP_N};
     }
-    MatrixView<T> SubMatrix(auto row, auto col, auto rows, auto cols) const
+    MatrixView<ValueType> SubMatrix(auto row, auto col, auto rows, auto cols) const
     {
-        return MatrixView<T>{data + ld * col + row, rows, cols, ld};
+        return MatrixView<ValueType>{const_cast<ValueType*>(data) + ld * col + row, rows, cols, ld};
     }
-    MatrixView<T> LeftCols(auto cols) const { return SubMatrix(0, 0, m, cols); }
-    MatrixView<T> RightCols(auto cols) const { return SubMatrix(0, Cols() - cols, m, cols); }
-    MatrixView<T> TopRows(auto rows) const { return SubMatrix(0, 0, rows, Cols()); }
-    MatrixView<T> BottomRows(auto rows) const { return SubMatrix(Rows() - rows, 0, rows, Cols()); }
-    MatrixView<T> Col(auto col) const { return SubMatrix(0, col, Rows(), Cols()); }
-    MatrixView<T> Row(auto row) const { return SubMatrix(row, 0, 1, Cols()); }
-    VectorView<T> Flattened() const { return VectorView<T>{data, Rows() * Cols(), 1}; }
+    MatrixView<ValueType> LeftCols(auto cols) const { return SubMatrix(0, 0, m, cols); }
+    MatrixView<ValueType> RightCols(auto cols) const
+    {
+        return SubMatrix(0, Cols() - cols, m, cols);
+    }
+    MatrixView<ValueType> TopRows(auto rows) const { return SubMatrix(0, 0, rows, Cols()); }
+    MatrixView<ValueType> BottomRows(auto rows) const
+    {
+        return SubMatrix(Rows() - rows, 0, rows, Cols());
+    }
+    MatrixView<ValueType> Col(auto col) const { return SubMatrix(0, col, Rows(), Cols()); }
+    MatrixView<ValueType> Row(auto row) const { return SubMatrix(row, 0, 1, Cols()); }
+    VectorView<ValueType> Flattened() const
+    {
+        return VectorView<ValueType>{const_cast<ValueType*>(data), Rows() * Cols(), 1};
+    }
 };
 
 template <class T>
 struct Matrix
 {
-    using ValueType = T;
+    using ValueType = std::remove_cvref_t<T>;
 
     Matrix() = default;
     Matrix(auto rows, auto cols) : data(rows * cols), m(rows) {}
 
-    common::Buffer<T> data; ///< `m x n` dense matrix coefficients in column-major order
-    int m;                  ///< Number of rows
+    common::Buffer<ValueType> data; ///< `m x n` dense matrix coefficients in column-major order
+    int m;                          ///< Number of rows
 
     // Storage information
-    T* Raw() { return data.Raw(); }
-    T const* Raw() const { return data.Raw(); }
+    ValueType* Raw() { return data.Raw(); }
+    ValueType const* Raw() const { return data.Raw(); }
     auto Rows() const { return m; }
     auto Cols() const { return static_cast<int>(data.Size()) / m; }
     auto LeadingDimensions() const { return m; }
     auto Operation() const { return CUBLAS_OP_N; }
 
     // Accessors
-    MatrixView<T> View() const { return MatrixView<T>{data.Raw(), m, Cols(), m}; }
-    MatrixView<T> SubMatrix(auto row, auto col, auto rows, auto cols) const
+    MatrixView<ValueType> View() const
     {
-        T* a = data.Raw();
-        return MatrixView<T>{a + m * col + row, rows, cols, m};
+        return MatrixView<ValueType>{const_cast<ValueType*>(data.Raw()), m, Cols(), m};
     }
-    MatrixView<T> LeftCols(auto cols) const { return SubMatrix(0, 0, m, cols); }
-    MatrixView<T> RightCols(auto cols) const { return SubMatrix(0, Cols() - cols, m, cols); }
-    MatrixView<T> TopRows(auto rows) const { return SubMatrix(0, 0, rows, Cols()); }
-    MatrixView<T> BottomRows(auto rows) const { return SubMatrix(Rows() - rows, 0, rows, Cols()); }
-    MatrixView<T> Col(auto col) const { return SubMatrix(0, col, Rows(), Cols()); }
-    MatrixView<T> Row(auto row) const { return SubMatrix(row, 0, 1, Cols()); }
-    VectorView<T> Flattened() const { return VectorView<T>{data.Raw(), Rows() * Cols(), 1}; }
+    MatrixView<ValueType> SubMatrix(auto row, auto col, auto rows, auto cols) const
+    {
+        ValueType* a = const_cast<ValueType*>(data.Raw());
+        return MatrixView<ValueType>{a + m * col + row, rows, cols, m};
+    }
+    MatrixView<ValueType> LeftCols(auto cols) const { return SubMatrix(0, 0, m, cols); }
+    MatrixView<ValueType> RightCols(auto cols) const
+    {
+        return SubMatrix(0, Cols() - cols, m, cols);
+    }
+    MatrixView<ValueType> TopRows(auto rows) const { return SubMatrix(0, 0, rows, Cols()); }
+    MatrixView<ValueType> BottomRows(auto rows) const
+    {
+        return SubMatrix(Rows() - rows, 0, rows, Cols());
+    }
+    MatrixView<ValueType> Col(auto col) const { return SubMatrix(0, col, Rows(), Cols()); }
+    MatrixView<ValueType> Row(auto row) const { return SubMatrix(row, 0, 1, Cols()); }
+    VectorView<ValueType> Flattened() const
+    {
+        return VectorView<ValueType>{const_cast<ValueType*>(data.Raw()), Rows() * Cols(), 1};
+    }
 };
 
 template <class T>
 struct Vector
 {
-    using ValueType = T;
+    using ValueType = std::remove_cvref_t<T>;
 
     Vector() = default;
     Vector(auto rows) : data(rows), n(rows) {}
 
-    common::Buffer<T> data; ///< `n x 1` dense vector coefficients
-    int n;                  ///< Number of rows
+    common::Buffer<ValueType> data; ///< `n x 1` dense vector coefficients
+    int n;                          ///< Number of rows
 
     // Storage information
-    T* Raw() { return data.Raw(); }
-    T const* Raw() const { return data.Raw(); }
+    ValueType* Raw() { return data.Raw(); }
+    ValueType const* Raw() const { return data.Raw(); }
     auto Rows() const { return n; }
     constexpr auto Cols() const { return 1; }
     constexpr auto Increment() const { return 1; }
 
     // Accessors
-    VectorView<T> Slice(auto row, auto rows, auto inc) const
+    VectorView<ValueType> Slice(auto row, auto rows, auto inc) const
     {
-        T* a = data.Raw();
-        return VectorView<T>{a + row, rows, inc};
+        ValueType* a = const_cast<ValueType*>(data.Raw());
+        return VectorView<ValueType>{a + row, rows, inc};
     }
-    VectorView<T> Segment(auto row, auto rows) const { return Slice(row, rows, 1); }
-    VectorView<T> Head(auto rows) const { return Slice(0, rows, 1); }
-    VectorView<T> Tail(auto rows) const { return Slice(n - rows, rows, 1); }
+    VectorView<ValueType> Segment(auto row, auto rows) const { return Slice(row, rows, 1); }
+    VectorView<ValueType> Head(auto rows) const { return Slice(0, rows, 1); }
+    VectorView<ValueType> Tail(auto rows) const { return Slice(n - rows, rows, 1); }
 };
 
 class Blas
