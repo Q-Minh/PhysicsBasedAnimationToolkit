857f9f0ccd57352a556f1926af8c1699a0b6443d
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2025-04-29 17:29:40 -0400

    Implement s-step backward differentiation schemes

---
+++ source/pbat/sim/integration/Bdf.cpp
@@ -0,0 +1,222 @@
+#include "Bdf.h"
+
+#include <algorithm>
+#include <exception>
+
+namespace pbat::sim::integration {
+
+Bdf::Bdf(int step, int order)
+    : xt(), xtilde(), ti(0), h(Scalar(0.01)), mOrder(), mStep(), mAlpha(), mBeta()
+{
+    SetStep(step);
+    SetOrder(order);
+}
+
+auto Bdf::State(int k, int o) const
+{
+    if (k < 0 || k > mStep)
+    {
+        throw std::out_of_range("0 <= k <= s");
+    }
+    if (o < 0 || o >= mOrder)
+    {
+        throw std::out_of_range("0 <= o < order");
+    }
+    auto nIterates = mStep + 1;
+    auto kt        = common::Modulo(ti, nIterates);
+    return xt.col(o * nIterates + kt);
+}
+
+auto Bdf::State(int k, int o)
+{
+    if (k < 0 || k > mStep)
+    {
+        throw std::out_of_range("0 <= k <= s");
+    }
+    if (o < 0 || o >= mOrder)
+    {
+        throw std::out_of_range("0 <= o < order");
+    }
+    auto nIterates = mStep + 1;
+    auto kt        = common::Modulo(ti /*- nIterates*/ + k, nIterates);
+    return xt.col(o * nIterates + kt);
+}
+
+auto Bdf::CurrentState(int o) const
+{
+    return State(mStep, o);
+}
+
+auto Bdf::CurrentState(int o)
+{
+    return State(mStep, o);
+}
+
+void Bdf::SetOrder(int order)
+{
+    if (order <= 0)
+    {
+        throw std::invalid_argument("order > 0");
+    }
+    mOrder = order;
+}
+
+void Bdf::SetStep(int step)
+{
+    if (step < 1 || step > 6)
+    {
+        throw std::invalid_argument("0 < s < 7.");
+    }
+    mStep = step;
+    mAlpha.setZero();
+    switch (mStep)
+    {
+        case 1:
+            mAlpha(0) = Scalar(-1);
+            mBeta     = Scalar(1);
+            break;
+        case 2:
+            mAlpha(0) = Scalar(1) / 3;
+            mAlpha(1) = Scalar(-4) / 3;
+            mBeta     = Scalar(2) / 3;
+            break;
+        case 3:
+            mAlpha(0) = Scalar(-2) / 11;
+            mAlpha(1) = Scalar(9) / 11;
+            mAlpha(2) = Scalar(-18) / 11;
+            mBeta     = Scalar(6) / 11;
+            break;
+        case 4:
+            mAlpha(0) = Scalar(3) / 25;
+            mAlpha(1) = Scalar(-16) / 25;
+            mAlpha(2) = Scalar(36) / 25;
+            mAlpha(3) = Scalar(-48) / 25;
+            mBeta     = Scalar(12) / 25;
+            break;
+        case 5:
+            mAlpha(0) = Scalar(-12) / 137;
+            mAlpha(1) = Scalar(75) / 137;
+            mAlpha(2) = Scalar(-200) / 137;
+            mAlpha(3) = Scalar(300) / 137;
+            mAlpha(4) = Scalar(-300) / 137;
+            mBeta     = Scalar(60) / 137;
+            break;
+        case 6:
+            mAlpha(0) = Scalar(10) / 147;
+            mAlpha(1) = Scalar(-72) / 147;
+            mAlpha(2) = Scalar(225) / 147;
+            mAlpha(3) = Scalar(-400) / 147;
+            mAlpha(4) = Scalar(450) / 147;
+            mAlpha(5) = Scalar(-360) / 147;
+            mBeta     = Scalar(60) / 147;
+            break;
+    }
+}
+
+void Bdf::SetTimeStep(Scalar dt)
+{
+    if (dt <= 0)
+    {
+        throw std::invalid_argument("dt > 0");
+    }
+    h = dt;
+}
+
+void Bdf::ConstructEquations()
+{
+    xtilde.setZero();
+    for (auto o = 0; o < mOrder; ++o)
+        for (auto k = 0; k < mStep; ++k)
+            xtilde.col(o) += mAlpha(k) * State(k, o);
+}
+
+} // namespace pbat::sim::integration
+
+#include <doctest/doctest.h>
+
+TEST_CASE("[sim][integration] Bdf")
+{
+    using namespace pbat;
+    using namespace pbat::sim::integration;
+
+    auto constexpr n = 5;
+    VectorX f(n);
+    f.setOnes();
+
+    SUBCASE("2nd order BDF1")
+    {
+        // Arrange
+        auto constexpr order  = 2;
+        auto constexpr step   = 1;
+        auto constexpr nsteps = 4;
+        Scalar constexpr h    = 0.01;
+        MatrixX x0(n, order);
+        x0.setRandom();
+
+        VectorX x(n);
+        VectorX v(n);
+        x = x0.col(0);
+        v = x0.col(1);
+        for (auto i = 0; i < nsteps; ++i)
+        {
+            v += h * f;
+            x += h * v;
+        }
+
+        // Act
+        Bdf bdf(step, order);
+        bdf.SetTimeStep(h);
+        bdf.SetInitialConditions(x0);
+        for (auto i = 0; i < nsteps; ++i)
+        {
+            bdf.Tick();
+            bdf.ConstructEquations();
+            bdf.CurrentState(1) = bdf.BetaTilde() * f - bdf.Inertia(1);
+            bdf.CurrentState(0) = bdf.BetaTilde() * bdf.CurrentState(1) - bdf.Inertia(0);
+        }
+
+        // Assert
+        CHECK(bdf.CurrentState(0).isApprox(x));
+        CHECK(bdf.CurrentState(1).isApprox(v));
+    }
+    SUBCASE("2nd order BDF2")
+    {
+        // Arrange
+        auto constexpr order  = 2;
+        auto constexpr step   = 2;
+        auto constexpr nsteps = 4;
+        Scalar constexpr h    = 0.01;
+        MatrixX x0(n, order);
+        x0.setRandom();
+
+        VectorX x(n), xtm1(n), xtm2(n);
+        VectorX v(n), vtm1(n), vtm2(n);
+        x = xtm1 = xtm2 = x0.col(0);
+        v = vtm1 = vtm2 = x0.col(1);
+        for (auto i = 0; i < nsteps; ++i)
+        {
+            v    = (Scalar(4) / 3) * vtm1 - (Scalar(1) / 3) * vtm2 + (Scalar(2) / 3) * h * f;
+            x    = (Scalar(4) / 3) * xtm1 - (Scalar(1) / 3) * xtm2 + (Scalar(2) / 3) * h * v;
+            vtm2 = vtm1;
+            vtm1 = v;
+            xtm2 = xtm1;
+            xtm1 = x;
+        }
+
+        // Act
+        Bdf bdf(step, order);
+        bdf.SetTimeStep(h);
+        bdf.SetInitialConditions(x0);
+        for (auto i = 0; i < nsteps; ++i)
+        {
+            bdf.Tick();
+            bdf.ConstructEquations();
+            bdf.CurrentState(1) = bdf.BetaTilde() * f - bdf.Inertia(1);
+            bdf.CurrentState(0) = bdf.BetaTilde() * bdf.CurrentState(1) - bdf.Inertia(0);
+        }
+
+        // Assert
+        CHECK(bdf.CurrentState(0).isApprox(x));
+        CHECK(bdf.CurrentState(1).isApprox(v));
+    }
+}
---
+++ source/pbat/sim/integration/Bdf.h
@@ -0,0 +1,222 @@
+/**
+ * @file Bdf.h
+ * @author Quoc-Minh Ton-That (tonthat.quocminh@gmail.com)
+ * @brief BDF (Backward Differentiation Formula) time integration scheme
+ * @date 2025-04-29
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#ifndef PBAT_SIM_INTEGRATION_BDF_H
+#define PBAT_SIM_INTEGRATION_BDF_H
+
+#include "pbat/Aliases.h"
+#include "pbat/common/Modulo.h"
+
+namespace pbat::sim::integration {
+
+/**
+ * @brief BDF (Backward Differentiation Formula) time integration scheme for a system of ODEs for an
+ * initial value problem (IVP)
+ *
+ * Refer to background [here](https://en.wikipedia.org/wiki/Backward_differentiation_formula).
+ *
+ * An order \f$ p \f$ system of ODEs \f$ x^{(p)} = f(t, \frac{d^{p-1}}{dt^{p-1}} x, \dots, x) \f$
+ * can be transformed into a system of \f$ p \f$ first-order ODEs using slack variables \f$ x^{(o)}
+ * = \frac{d^o}{dt^o} x \f$ for \f$ o = 0, \dots, p-1 \f$, such that the IVP can be rewritten as
+ * \f[
+ * \begin{align*}
+ * \frac{d}{dt} x_{p-1} &= f(t, x^{(p-1)}, \dots, x^{(o)}) \\
+ * \frac{d}{dt} x^{(p-j)} &= x^{(p-j+1)}, \quad j = 2, \dots, p
+ * \end{align*} .
+ * \f]
+ *
+ * Since BDF discretizes each equation as \f$ \sum_{k=0}^s \alpha_k x_{n-s-1+k} = h \beta f(t,
+ * x_n^{(p-1)}, \dots, x_n) \f$, we have that
+ * \f[
+ * \begin{align*}
+ * x^{(p-1)}_n + \tilde{x}^{(p-1)}_\text{BDFS} - \tilde{\beta}_\text{BDFS} f(t, x_n^{(p-1)}, \dots,
+ * x_n) &= 0 \\ x^{(o)}_n + \tilde{x}^{(o)}_\text{BDFS} - \tilde{\beta}_\text{BDFS} x^{(o+1)}_n &= 0
+ * \end{align*}
+ * \f]
+ * for \f$ o=0,\dots,p-2 \f$, where \f$ \tilde{\beta} = h \beta \f$ and \f$ n \f$ is the next time
+ * step index.
+ *
+ * One performs time integration by solving for the states and their derivatives \f$ x_n^{(o)} \f$
+ * for
+ * \f$ o=0,\dots,p-1 \f$ at each time step using root-finding of the above equations. Alternatively,
+ * the root-finding equations can be treated as stationarity conditions \f$ \nabla f = 0 \f$ for
+ * some objective function \f$ f \f$, and solved via numerical optimization of \f$ f \f$.
+ *
+ * This class encapsulates the construction of the so-called inertias \f$
+ * \tilde{x}^{(o)}_\text{BDFS} \f$, as well as the interpolation coefficients \f$ \alpha_k \f$ and
+ * forcing term coefficient \f$ \beta \f$ and generalize the BDF scheme to various ODE systems of
+ * different orders. Specific ODEs are then entirely defined by the forcing function \f$ f(t,
+ * \frac{d^{p-1}}{dt^{p-1}} x, \dots, x) \f$, and it is up to the user to derive their specific
+ * equations to solve.
+ *
+ * ```
+ * bdf.SetInitialConditions(x0)
+ * for ti in steps:
+ *   bdf.Tick()
+ *   bdf.ConstructEquations()
+ *   bdf.CurrentState(o) = userSolve(bdf, o)
+ * ```
+ * 
+ */
+class Bdf
+{
+  public:
+    /**
+     * @brief Construct a `step`-step BDF (backward differentiation formula) time integration scheme
+     * for a system of ODEs (ordinary differential equation) of order `order`.
+     * @param step `0 < s < 7` backward differentiation scheme
+     * @param order `order > 0` order of the ODE
+     * @pre `0 < step < 7`
+     * @pre `order > 0`
+     */
+    Bdf(int step = 1, int order = 2);
+
+    MatrixX xt; ///< `n x |(s+1)*order|` matrix of `n`-dimensional states and their time derivatives
+                ///< s.t. \f$ xt.col(o*s + k) = x^(k)(t - k*dt) \f$ for \f$ k = 0, ..., s \f$ and
+                ///< \f$ o = 0, ..., \text{order}-1 \f$
+    MatrixX
+        xtilde; ///< `n x order` matrix of `n`-dimensional aggregated past states and time
+                ///< derivatives s.t. xtilde.col(o) = \f$ \frac{1}{\alpha_s} \sum_{k=t_i-s}^{s-1}
+                ///< \alpha_k x_k \f$ for \f$ o = 0, ..., \text{order}-1 \f$
+    Index ti;   ///< Current time index s.t. \f$t = t_0 + h t_i\f$
+    Scalar h;   ///< Time step size \f$ h \f$
+
+    /**
+     * @brief Order of the ODE
+     * @return Order of the ODE
+     */
+    [[maybe_unused]] auto Order() const { return mOrder; }
+    /**
+     * @brief Step `s` of the `s`-step BDF scheme
+     * @return Step `s` of the `s`-step BDF scheme
+     */
+    [[maybe_unused]] auto Step() const { return mStep; }
+    /**
+     * @brief Number of iterates in the BDF scheme
+     * @return Number of iterates in the BDF scheme
+     */
+    [[maybe_unused]] auto Iterates() const { return mStep + 1; }
+    /**
+     * @brief Number of ODEs
+     * @return Number of ODEs
+     */
+    [[maybe_unused]] auto Dimensions() const { return xt.rows(); }
+    /**
+     * @brief Time step size
+     * @return Time step size
+     */
+    [[maybe_unused]] auto TimeStep() const { return h; }
+    /**
+     * @brief Inertia of the BDF scheme for the \f$ o^\text{th} \f$ state derivative
+     * @param o Order of the state derivative \f$ o = 0, ..., \text{order}-1 \f$
+     * @return `n x 1` inertia vector for the \f$ o^\text{th} \f$ state derivative
+     */
+    [[maybe_unused]] auto Inertia(int o) const { return xtilde.col(o); }
+    /**
+     * @brief \f$ o^\text{th} \f$ state derivative
+     * @param k State index \f$ k = 0, ..., s \f$ for the vector \f$ x^{(o)}_{t_i - s-1 + k} \f$
+     * @param o Order of the state derivative \f$ o = 0, ..., \text{order}-1 \f$
+     * @return `n x 1` state derivative vector \f$ x^{(o)}_{t_i - s-1 + k} \f$
+     */
+    auto State(int k, int o = 0) const;
+    /**
+     * @brief \f$ o^\text{th} \f$ state derivative
+     * @param k State index \f$ k = 0, ..., s \f$ for the vector \f$ x^{(o)}_{t_i - s-1 + k} \f$
+     * @param o Order of the state derivative \f$ o = 0, ..., \text{order}-1 \f$
+     * @return `n x 1` state derivative vector \f$ x^{(o)}_{t_i - s-1 + k} \f$
+     */
+    auto State(int k, int o = 0);
+    /**
+     * @brief Current state derivative
+     * @param o Order of the state derivative \f$ o = 0, ..., \text{order}-1 \f$
+     * @return `n x 1` current state derivative vector \f$ x^{(o)}_{t_i} \f$
+     */
+    auto CurrentState(int o = 0) const;
+    /**
+     * @brief Current state derivative
+     * @param o Order of the state derivative \f$ o = 0, ..., \text{order}-1 \f$
+     * @return `n x 1` current state derivative vector \f$ x^{(o)}_{t_i} \f$
+     */
+    auto CurrentState(int o = 0);
+    /**
+     * @brief Interpolation coefficients \f$ \alpha_k \f$ except \f$ \alpha_s \f$
+     * @return `s x 1` vector of interpolation coefficients \f$ \alpha_k \f$
+     */
+    [[maybe_unused]] auto Alpha() const { return mAlpha(Eigen::seqN(0, mStep)); }
+    /**
+     * @brief Forcing term coefficient \f$ \beta \f$ s.t. \f$ \tilde{\beta} = h \beta \f$
+     * @return Forcing term coefficient \f$ \beta \f$
+     */
+    [[maybe_unused]] auto Beta() const { return mBeta; }
+    /**
+     * @brief Time-step scaled forcing term coefficient \f$ \tilde{\beta} = h \beta \f$
+     * @return Time-step scaled forcing term coefficient \f$ \tilde{\beta} = h \beta \f$
+     */
+    [[maybe_unused]] auto BetaTilde() const { return mBeta * h; }
+
+    /**
+     * @brief Set the order of the ODE system
+     * @param order Order of the ODE system \f$ \text{order} > 0 \f$
+     * @pre `order > 0`
+     */
+    void SetOrder(int order);
+    /**
+     * @brief Set the step of the BDF scheme
+     * @param step Step of the BDF scheme \f$ 0 < s < 7 \f$
+     * @pre `0 < step < 7`
+     */
+    void SetStep(int step);
+    /**
+     * @brief Set the time step size
+     * @param dt Time step size \f$ dt > 0 \f$
+     * @pre `dt > 0`
+     */
+    void SetTimeStep(Scalar dt);
+    /**
+     * @brief Construct the BDF equations, i.e. compute \f$ \tilde{x^{(o)}} = \sum_{k=0}^{s-1}
+     * \alpha_k x^{(o)}_{t_i - s-1 + k} \f$ for all \f$ o = 0, ..., \text{order}-1 \f$
+     */
+    void ConstructEquations();
+    /**
+     * @brief Set the initial conditions for the initial value problem
+     * @param x0 `n x \text{order}` matrix of initial conditions s.t. `x0.col(o) = \f$ x^{(o)}_{t_0}
+     * \f$` for \f$ o = 0, ..., \text{order}-1 \f$
+     * @pre `x0.cols() == order`
+     * @post `ti == 0`
+     */
+    template <class TDerivedX>
+    void SetInitialConditions(Eigen::DenseBase<TDerivedX> const& x0);
+    /**
+     * @brief Advance the BDF scheme by one time step
+     */
+    [[maybe_unused]] void Tick() { ++ti; }
+
+  private:
+    int mOrder;       ///< ODE order \f$ \text{order} >= 1 \f$
+    int mStep;        ///< Step \f$ 0 < s < 7 \f$ backward differentiation scheme
+    Vector<6> mAlpha; ///< Interpolation coefficients \f$ \alpha_k \f$ except \f$ \alpha_s \f$
+    Scalar mBeta; ///< Forcing term coefficient \f$ \beta \f$ s.t. \f$ \tilde{\beta} = h \beta \f$
+};
+
+template <class TDerivedX>
+inline void Bdf::SetInitialConditions(Eigen::DenseBase<TDerivedX> const& x0)
+{
+    ti             = 0;
+    auto n         = x0.rows();
+    auto nIterates = mStep + 1;
+    xt.resize(n, nIterates * mOrder);
+    xtilde.resize(n, mOrder);
+    for (auto o = 0; o < mOrder; ++o)
+        xt.middleCols(o * nIterates, nIterates).colwise() = x0.col(o);
+}
+
+} // namespace pbat::sim::integration
+
+#endif // PBAT_SIM_INTEGRATION_BDF_H
