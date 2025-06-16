aebded1092b016bd793b8e32acdf3fbc3f01575c
Q-Minh <tonthat.quocminh@gmail.com>
2025-06-15 19:07:38 -0700

    Update higher order elasticity example

--- python/examples/elasticity_higher_order.py
+++ python/examples/elasticity_higher_order.py
@@ -11,75 +11,138 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Simple 3D elastic simulation using quadratic FEM tetrahedra",
     )
-    parser.add_argument("-i", "--input", help="Path to input mesh", type=str,
-                        dest="input", required=True)
-    parser.add_argument("-r", "--refined-input", help="Path to refined input mesh", type=str,
-                        dest="rinput", required=True)
-    parser.add_argument("-m", "--mass-density", help="Mass density", type=float,
-                        dest="rho", default=1000.)
-    parser.add_argument("-Y", "--young-modulus", help="Young's modulus", type=float,
-                        dest="Y", default=1e6)
-    parser.add_argument("-n", "--poisson-ratio", help="Poisson's ratio", type=float,
-                        dest="nu", default=0.45)
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="Path to input mesh",
+        type=str,
+        dest="input",
+        required=True,
+    )
+    parser.add_argument(
+        "-r",
+        "--refined-input",
+        help="Path to refined input mesh",
+        type=str,
+        dest="rinput",
+        required=True,
+    )
+    parser.add_argument(
+        "-m",
+        "--mass-density",
+        help="Mass density",
+        type=float,
+        dest="rho",
+        default=1000.0,
+    )
+    parser.add_argument(
+        "-Y",
+        "--young-modulus",
+        help="Young's modulus",
+        type=float,
+        dest="Y",
+        default=1e6,
+    )
+    parser.add_argument(
+        "-n",
+        "--poisson-ratio",
+        help="Poisson's ratio",
+        type=float,
+        dest="nu",
+        default=0.45,
+    )
     args = parser.parse_args()
 
     # Load domain and visual meshes
     imesh = meshio.read(args.input)
     V, C = imesh.points, imesh.cells_dict["tetra"]
-    V = V.astype(np.float64, order='c')
-    C = C.astype(np.int64, order='c')
+    V = V.astype(np.float64, order="c")
+    C = C.astype(np.int64, order="c")
     rimesh = meshio.read(args.rinput)
     VR, FR = rimesh.points, rimesh.cells_dict["triangle"]
-    VR = VR.astype(np.float64, order='c')
-    FR = FR.astype(np.int64, order='c')
+    VR = VR.astype(np.float64, order="c")
+    FR = FR.astype(np.int64, order="c")
 
     # Construct FEM quantities for simulation
-    mesh = pbat.fem.Mesh(
-        V.T, C.T, element=pbat.fem.Element.Tetrahedron, order=2)
-    x = mesh.X.reshape(math.prod(mesh.X.shape), order='f')
+    element = pbat.fem.Element.Tetrahedron
+    order = 2
+    X, E = pbat.fem.mesh(V.T, C.T, element=element, order=order)
+    dims = X.shape[0]
+    x = X.reshape(math.prod(X.shape), order="f")
     v = np.zeros(x.shape[0])
-    rho = args.rho
 
     # Mass matrix
-    M, detJeM = pbat.fem.mass_matrix(mesh, rho=rho, quadrature_order=4)
+    rho = args.rho
+    M = pbat.fem.mass_matrix(E, X, rho=rho, dims=dims, element=element, order=order)
     Minv = pbat.math.linalg.ldlt(M)
     Minv.compute(M)
 
     # Construct load vector from gravity field
-    g = np.zeros(mesh.dims)
+    g = np.zeros(dims)
     g[-1] = -9.81
-    f, detJeF = pbat.fem.load_vector(mesh, rho*g, quadrature_order=2)
-    a = Minv.solve(f).squeeze()
+    fe = rho * g
+    f = pbat.fem.load_vector(E, X, fe, element=element, order=order)
+    a = Minv.solve(np.ravel(f, order="F")).squeeze()
 
     # Create hyper elastic potential
     Y, nu, psi = args.Y, args.nu, pbat.fem.HyperElasticEnergy.StableNeoHookean
-    hep, egU, wgU, GNeU, lameg = pbat.fem.hyper_elastic_potential(
-        mesh, Y=Y, nu=nu, energy=psi, quadrature_order=4)
+    mu, llambda = pbat.fem.lame_coefficients(Y, nu)
+    wg = pbat.fem.mesh_quadrature_weights(
+        E, X, element, order=order, quadrature_order=order
+    )
+    eg = pbat.fem.mesh_quadrature_elements(E, wg)
+    mug = np.full(math.prod(wg.shape), mu, dtype=x.dtype)
+    lambdag = np.full(math.prod(wg.shape), llambda, dtype=x.dtype)
+    GNegU = pbat.fem.shape_function_gradients(
+        E, X, element=element, dims=dims, order=order
+    )
+    ElasticityComputationFlags = pbat.fem.ElementElasticityComputationFlags
+    spd_correction = pbat.fem.HyperElasticSpdCorrection.Absolute
+    energy = pbat.fem.HyperElasticEnergy.StableNeoHookean
 
     # Set Dirichlet boundary conditions
-    Xmin = mesh.X.min(axis=1)
-    Xmax = mesh.X.max(axis=1)
-    Xmax[0] = Xmin[0]+1e-4
-    Xmin[0] = Xmin[0]-1e-4
+    Xmin = X.min(axis=1)
+    Xmax = X.max(axis=1)
+    Xmax[0] = Xmin[0] + 1e-4
+    Xmin[0] = Xmin[0] - 1e-4
     aabb = pbat.geometry.aabb(np.vstack((Xmin, Xmax)).T)
-    vdbc = aabb.contained(mesh.X)
+    vdbc = aabb.contained(X)
     dbcs = np.array(vdbc)[:, np.newaxis]
-    dbcs = np.repeat(dbcs, mesh.dims, axis=1)
-    for d in range(mesh.dims):
-        dbcs[:, d] = mesh.dims*dbcs[:, d]+d
+    dbcs = np.repeat(dbcs, dims, axis=1)
+    for d in range(dims):
+        dbcs[:, d] = dims * dbcs[:, d] + d
     dbcs = dbcs.reshape(math.prod(dbcs.shape))
     n = x.shape[0]
     dofs = np.setdiff1d(list(range(n)), dbcs)
 
     # Setup linear solver
-    Hdd = hep.to_matrix()[:, dofs].tocsr()[dofs, :]
+    # ERROR: There is a bug with higher order hyper elastic potential,
+    # but will fix it later.
+    H = pbat.fem.hyper_elastic_potential(
+        E,
+        X.shape[1],
+        eg=np.ravel(eg),
+        wg=np.ravel(wg),
+        GNeg=GNegU,
+        mug=mug,
+        lambdag=lambdag,
+        x=x,
+        energy=energy,
+        flags=ElasticityComputationFlags.Hessian,
+        spd_correction=spd_correction,
+        element=element,
+        order=order,
+        dims=dims,
+    )
+    Hdd = H.tocsc()[:, dofs].tocsr()[dofs, :]
     Hddinv = pbat.math.linalg.ldlt(Hdd)
     Hddinv.analyze(Hdd)
 
     # Setup visual mesh
     bvh = pbat.geometry.bvh(V.T, C.T, cell=pbat.geometry.Cell.Tetrahedron)
     e, d = bvh.nearest_primitives_to_points(VR.T)
-    phi = pbat.fem.shape_functions_at(mesh, e, VR.T)
+    Xi = pbat.fem.reference_positions(E, X, e, VR.T, element=element, order=order)
+    phi = pbat.fem.shape_functions_at(Xi, element=element, order=order)
 
     ps.set_verbosity(0)
     ps.set_up_dir("z_up")
@@ -90,7 +153,7 @@ if __name__ == "__main__":
     ps.init()
     vm = ps.register_volume_mesh("Domain", V, C)
     sm = ps.register_surface_mesh("Visual", VR, FR)
-    pc = ps.register_point_cloud("Dirichlet", mesh.X[:, vdbc].T)
+    pc = ps.register_point_cloud("Dirichlet", X[:, vdbc].T)
     dt = 0.01
     animate = False
     dx = np.zeros(n)
@@ -106,12 +169,27 @@ if __name__ == "__main__":
         if animate or step:
             profiler.begin_frame("Physics")
             # 1 Newton step
-            hep.compute_element_elasticity(x, grad=True, hessian=True)
-            gradU, HU = hep.gradient(), hep.hessian()
+            gradU, HU = pbat.fem.hyper_elastic_potential(
+                E,
+                X.shape[1],
+                eg=np.ravel(eg),
+                wg=np.ravel(wg),
+                GNeg=GNegU,
+                mug=mug,
+                lambdag=lambdag,
+                x=x,
+                energy=energy,
+                flags=ElasticityComputationFlags.Gradient
+                | ElasticityComputationFlags.Hessian,
+                spd_correction=spd_correction,
+                element=element,
+                order=order,
+                dims=dims,
+            )
             dt2 = dt**2
-            xtilde = x + dt*v + dt2*a
+            xtilde = x + dt * v + dt2 * a
             A = M + dt2 * HU
-            b = -(M @ (x - xtilde) + dt2*gradU)
+            b = -(M @ (x - xtilde) + dt2 * gradU)
             Add = A.tocsc()[:, dofs].tocsr()[dofs, :]
             bd = b[dofs]
             Hddinv.factorize(Add)
@@ -121,8 +199,8 @@ if __name__ == "__main__":
             profiler.end_frame("Physics")
 
             # Update visuals
-            X = x.reshape(mesh.X.shape[0], mesh.X.shape[1], order='f')
-            xv = (X[:, mesh.E[:, e]] * phi).sum(axis=1)
+            V = x.reshape(X.shape[0], X.shape[1], order="f")
+            xv = (V[:, E[:, e]] * phi).sum(axis=1)
             sm.update_vertex_positions(xv.T)
 
     ps.set_user_callback(callback)
