0d60917956aaf0b27311c817a49b39c98a38619a
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2025-09-17 09:54:36 -0400

    Bind new templated Bdf

--- bindings/pypbat/sim/integration/Bdf.cpp
+++ bindings/pypbat/sim/integration/Bdf.cpp
@@ -8,10 +8,10 @@ namespace pbat::py::sim::integration {
 void BindBdf(pybind11::module& m)
 {
     namespace pyb = pybind11;
-
-    pyb::class_<pbat::sim::integration::Bdf>(m, "Bdf")
+    using BdfType = pbat::sim::integration::Bdf<Scalar>;
+    pyb::class_<BdfType>(m, "Bdf")
         .def(
-            pyb::init([](int step, int order) { return pbat::sim::integration::Bdf(step, order); }),
+            pyb::init([](int step, int order) { return BdfType(step, order); }),
             pyb::arg("step")  = 1,
             pyb::arg("order") = 2,
             "Construct a `step`-step BDF (backward differentiation formula) time integration "
@@ -22,30 +22,17 @@ void BindBdf(pybind11::module& m)
             "    order (int): `order > 0` order of the ODE\n"
             "Returns:\n"
             "    Bdf: BDF time integration scheme")
-        .def_property(
-            "order",
-            &pbat::sim::integration::Bdf::Order,
-            &pbat::sim::integration::Bdf::SetOrder,
-            "Order of the ODE")
+        .def_property("order", &BdfType::Order, &BdfType::SetOrder, "Order of the ODE")
         .def_property(
             "s",
-            [](pbat::sim::integration::Bdf& self) { return self.Step(); },
-            &pbat::sim::integration::Bdf::SetStep,
+            [](BdfType& self) { return self.Step(); },
+            &BdfType::SetStep,
             "Step `s` of the `s`-step BDF scheme")
-        .def_property(
-            "h",
-            &pbat::sim::integration::Bdf::TimeStep,
-            &pbat::sim::integration::Bdf::SetTimeStep,
-            "Time step size")
-        .def_readonly(
-            "ti",
-            &pbat::sim::integration::Bdf::ti,
-            "Current time index s.t. `t = t0 + h ti`")
+        .def_property("h", &BdfType::TimeStep, &BdfType::SetTimeStep, "Time step size")
+        .def_readonly("ti", &BdfType::ti, "Current time index s.t. `t = t0 + h ti`")
         .def(
             "state",
-            [](pbat::sim::integration::Bdf& self, int k, int o) -> VectorX {
-                return self.State(k, o);
-            },
+            [](BdfType& self, int k, int o) -> VectorX { return self.State(k, o); },
             pyb::arg("k"),
             pyb::arg("o") = 0,
             "o^th state derivative\n"
@@ -56,9 +43,7 @@ void BindBdf(pybind11::module& m)
             "    n x 1: state derivative vector `x^(o)(ti - s + k)`")
         .def(
             "current_state",
-            [](pbat::sim::integration::Bdf& self, int o) -> VectorX {
-                return self.CurrentState(o);
-            },
+            [](BdfType& self, int o) -> VectorX { return self.CurrentState(o); },
             pyb::arg("o") = 0,
             "o^th most recent state derivative\n"
             "Args:\n"
@@ -67,7 +52,7 @@ void BindBdf(pybind11::module& m)
             "    n x 1: state derivative vector `x^(o)(ti - s + k)`")
         .def(
             "inertia",
-            [](pbat::sim::integration::Bdf& self, int o) { return self.Inertia(o); },
+            [](BdfType& self, int o) { return self.Inertia(o); },
             pyb::arg("o") = 0,
             "Inertia of the BDF scheme for the o^th state derivative\n"
             "Args:\n"
@@ -76,9 +61,7 @@ void BindBdf(pybind11::module& m)
             "    n x 1: inertia vector for the o^th state derivative")
         .def(
             "set_initial_conditions",
-            [](pbat::sim::integration::Bdf& self, pyb::EigenDRef<MatrixX> x0) {
-                self.SetInitialConditions(x0);
-            },
+            [](BdfType& self, pyb::EigenDRef<MatrixX> x0) { self.SetInitialConditions(x0); },
             pyb::arg("x0"),
             "Set the initial conditions for the initial value problem\n"
             "Args:\n"
@@ -89,15 +72,14 @@ void BindBdf(pybind11::module& m)
             "    None")
         .def(
             "construct_equations",
-            &pbat::sim::integration::Bdf::ConstructEquations,
+            &BdfType::ConstructEquations,
             "Construct the BDF equations, i.e. compute `x^(o) = sum_{k=0}^{s-1} alpha_k x^(o)(ti - "
-            "s + "
-            "k)` for all `o = 0, ..., order - 1`\n"
+            "s + k)` for all `o = 0, ..., order - 1`\n"
             "Returns:\n"
             "    None")
         .def(
             "step",
-            [](pbat::sim::integration::Bdf& self, pyb::EigenDRef<MatrixX> x) { self.Step(x); },
+            [](BdfType& self, pyb::EigenDRef<MatrixX> x) { self.Step(x); },
             pyb::arg("x"),
             "Advance the BDF scheme by one time step\n"
             "Args:\n"
@@ -107,7 +89,7 @@ void BindBdf(pybind11::module& m)
             "    None")
         .def(
             "serialize",
-            &pbat::sim::integration::Bdf::Serialize,
+            &BdfType::Serialize,
             pyb::arg("archive"),
             "Serialize the BDF scheme\n"
             "Args:\n"
@@ -116,7 +98,7 @@ void BindBdf(pybind11::module& m)
             "    None")
         .def(
             "deserialize",
-            &pbat::sim::integration::Bdf::Deserialize,
+            &BdfType::Deserialize,
             pyb::arg("archive"),
             "Deserialize the BDF scheme\n"
             "Args:\n"
