Fix outstanding shape function gradient error

python/examples/elasticity_higher_order.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     mug = np.full(math.prod(wg.shape), mu, dtype=x.dtype)
     lambdag = np.full(math.prod(wg.shape), llambda, dtype=x.dtype)
     GNegU = pbat.fem.shape_function_gradients(
-        E, X, element=element, dims=dims, order=order
+        E, X, element=element, dims=dims, order=order, quadrature_order=order
     )
     ElasticityComputationFlags = pbat.fem.ElementElasticityComputationFlags
     spd_correction = pbat.fem.HyperElasticSpdCorrection.Absolute
@@ -116,24 +116,23 @@ if __name__ == "__main__":
     dofs = np.setdiff1d(list(range(n)), dbcs)
 
     # Setup linear solver
-    # ERROR: There is a bug with higher order hyper elastic potential,
-    # but will fix it later.
-    _, _, H = pbat.fem.hyper_elastic_potential(
+    U, _, H = pbat.fem.hyper_elastic_potential(
         E,
         X.shape[1],
-        eg=np.ravel(eg),
-        wg=np.ravel(wg),
+        eg=np.ravel(eg, order="F"),
+        wg=np.ravel(wg, order="F"),
         GNeg=GNegU,
         mug=mug,
         lambdag=lambdag,
         x=x,
         energy=energy,
-        flags=ElasticityComputationFlags.Hessian,
+        flags=ElasticityComputationFlags.Potential | ElasticityComputationFlags.Hessian,
         spd_correction=spd_correction,
         element=element,
         order=order,
         dims=dims,
     )
+    print("Rest energy U={}".format(U))
     Hdd = H.tocsc()[:, dofs].tocsr()[dofs, :]
     Hddinv = pbat.math.linalg.ldlt(Hdd)
     Hddinv.analyze(Hdd)
@@ -172,12 +171,12 @@ if __name__ == "__main__":
             _, gradU, HU = pbat.fem.hyper_elastic_potential(
                 E,
                 X.shape[1],
-                eg=np.ravel(eg),
-                wg=np.ravel(wg),
+                eg=np.ravel(eg, order="F"),
+                wg=np.ravel(wg, order="F"),
                 GNeg=GNegU,
-                mug=mug,
-                lambdag=lambdag,
-                x=x,
+                mug=np.ravel(mug, order="F"),
+                lambdag=np.ravel(lambdag, order="F"),
+                x=np.ravel(x, order="F"),
                 energy=energy,
                 flags=ElasticityComputationFlags.Gradient
                 | ElasticityComputationFlags.Hessian,

python/examples/laplace.py
@@ -6,11 +6,15 @@ import numpy as np
 import argparse
 
 
-def harmonic_field(V: np.ndarray, C: np.ndarray, element: pbat.fem.Element, order: int, eps: float = 0.1):
+def harmonic_field(
+    V: np.ndarray,
+    C: np.ndarray,
+    element: pbat.fem.Element,
+    order: int,
+    eps: float = 0.1,
+):
     # Construct order order mesh and its Laplacian
-    X, E = pbat.fem.mesh(
-        V.T, C.T, element=element, order=order)
-    quadrature_order = max(int(2*(order-1)), 1)
+    X, E = pbat.fem.mesh(V.T, C.T, element=element, order=order)
     L = pbat.fem.laplacian_matrix(E, X, element=element, order=order)
     # Set Dirichlet boundary conditions at bottom and top of the model
     Xmin = X.min(axis=1)
@@ -18,10 +22,12 @@ def harmonic_field(V: np.ndarray, C: np.ndarray, element: pbat.fem.Element, orde
     extents = Xmax - Xmin
     extents[:-1] = 0
     aabblo = pbat.geometry.aabb(
-        np.vstack((Xmin - eps*extents, Xmax - (1-eps)*extents)).T)
+        np.vstack((Xmin - eps * extents, Xmax - (1 - eps) * extents)).T
+    )
     gammalo = aabblo.contained(X)
     aabbhi = pbat.geometry.aabb(
-        np.vstack((Xmin + (1-eps)*extents, Xmax + eps*extents)).T)
+        np.vstack((Xmin + (1 - eps) * extents, Xmax + eps * extents)).T
+    )
     gammahi = aabbhi.contained(X)
     gamma = np.concatenate((gammalo, gammahi))
     uk = np.concatenate((np.ones(len(gammalo)), np.zeros(len(gammahi))))
@@ -46,23 +52,42 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Higher order FEM demo",
     )
-    parser.add_argument("-i", "--input", help="Path to input tetrahedral mesh", type=str,
-                        dest="input", required=True)
-    parser.add_argument("-r", "--refined-input", help="Path to refined input tetrahedral mesh", type=str,
-                        dest="rinput", required=True)
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="Path to input tetrahedral mesh",
+        type=str,
+        dest="input",
+        required=True,
+    )
+    parser.add_argument(
+        "-r",
+        "--refined-input",
+        help="Path to refined input tetrahedral mesh",
+        type=str,
+        dest="rinput",
+        required=True,
+    )
     args = parser.parse_args()
 
     imesh = meshio.read(args.input)
-    V, C, = imesh.points, imesh.cells_dict["tetra"]
-    V = V.astype(np.float64, order='c')
-    C = C.astype(np.int64, order='c')
+    (
+        V,
+        C,
+    ) = (
+        imesh.points,
+        imesh.cells_dict["tetra"],
+    )
+    V = V.astype(np.float64, order="c")
+    C = C.astype(np.int64, order="c")
     element = pbat.fem.Element.Tetrahedron
     u1, X1, E1 = harmonic_field(V, C, element, order=1)
     u2, X2, E2 = harmonic_field(V, C, element, order=2)
     bvh = pbat.geometry.bvh(V.T, C.T, cell=pbat.geometry.Cell.Tetrahedron)
     rmesh = meshio.read(args.rinput)
-    Vrefined, Crefined = rmesh.points.astype(
-        np.float64, order='c'), rmesh.cells_dict["tetra"].astype(np.int64, order='c')
+    Vrefined, Crefined = rmesh.points.astype(np.float64, order="c"), rmesh.cells_dict[
+        "tetra"
+    ].astype(np.int64, order="c")
     Frefined = igl.boundary_facets(Crefined)
     Frefined[:, :2] = np.roll(Frefined[:, :2], shift=1, axis=1)
 
@@ -79,19 +104,21 @@ if __name__ == "__main__":
     ps.set_ground_plane_mode("shadow_only")
     ps.init()
     vm = ps.register_volume_mesh("domain refined", Vrefined, Crefined)
-    vm.add_scalar_quantity("Order 1 harmonic solution",
-                           u1ref, enabled=True, cmap="turbo")
+    vm.add_scalar_quantity(
+        "Order 1 harmonic solution", u1ref, enabled=True, cmap="turbo"
+    )
     vm.add_scalar_quantity("Order 2 harmonic solution", u2ref, cmap="turbo")
     niso = 15
 
     def isolines(V, F, u, niso):
         # Code for libigl 2.5.1
-        diso = (u.max() - u.min()) / (niso+2)
-        isovalues = np.array([(i+1)*diso for i in range(niso)])
+        diso = (u.max() - u.min()) / (niso + 2)
+        isovalues = np.array([(i + 1) * diso for i in range(niso)])
         Viso, Eiso, Iiso = igl.isolines(V, F, u, isovalues)
         # Uncomment for libigl 2.4.1
         # Viso1, Eiso1 = igl.isolines(V, F, u, niso)
         return Viso, Eiso
+
     Viso1, Eiso1 = isolines(Vrefined, Frefined, u1ref, niso)
     Viso2, Eiso2 = isolines(Vrefined, Frefined, u2ref, niso)
     cn1 = ps.register_curve_network("Order 1 contours", Viso1, Eiso1)