943f5457ad3c460fd8d61fe1015e2d973d1b390b
Q-Minh <tonthat.quocminh@gmail.com>
2025-10-17 22:46:27 -0400

    Refactor Chebyshev and Core VBD APIs to include iteration parameter in Step functions and remove unused ResetTransientData method

--- source/pbat/sim/algorithm/vbd/Chebyshev.h
+++ source/pbat/sim/algorithm/vbd/Chebyshev.h
@@ -23,13 +23,8 @@ struct ChebyshevParams
 {
     Scalar rho; ///< Spectral radius estimate
     /**
-     * @brief Reset transient data to start a new solve
-     */
-    void ResetTransientData() { k = 0; }
-    /**
      * @brief Read/Write parameters
      */
-    Index k{0};   ///< Current iteration
     Scalar omega; ///< Chebyshev omega parameter
     Eigen::Matrix<Scalar, 3, Eigen::Dynamic>
         xkm1; ///< `3 x |# verts|` \f$ x^{k-1} \f$ used in Chebyshev semi-iterative method
@@ -38,7 +33,7 @@ struct ChebyshevParams
 };
 
 /**
- * @brief One Chebyshev accelerated VBD minimization step
+ * @brief Initialize Chebyshev accelerated VBD minimization solve
  * @tparam TElasticEnergy Hyper-elastic energy model
  * @param fem Finite element elasto dynamics problem (in/out parameter)
  * @param params Solver parameters
@@ -46,18 +41,51 @@ struct ChebyshevParams
  * @pre `TElasticEnergy::kDims == 3`
  */
 template <physics::CHyperElasticEnergy TElasticEnergy>
-void Step(FemElastoDynamics<TElasticEnergy>& fem, Params const& params, ChebyshevParams& cheb);
+void InitializeSolve(
+    FemElastoDynamics<TElasticEnergy>& fem,
+    Params const& params,
+    ChebyshevParams& cheb);
+
+/**
+ * @brief One Chebyshev accelerated VBD minimization step
+ * @tparam TElasticEnergy Hyper-elastic energy model
+ * @param k Iteration
+ * @param fem Finite element elasto dynamics problem (in/out parameter)
+ * @param params Solver parameters
+ * @param cheb Chebyshev parameters
+ * @pre `TElasticEnergy::kDims == 3`
+ */
+template <physics::CHyperElasticEnergy TElasticEnergy>
+void Step(
+    Index k,
+    FemElastoDynamics<TElasticEnergy>& fem,
+    Params const& params,
+    ChebyshevParams& cheb);
 
 template <physics::CHyperElasticEnergy TElasticEnergy>
-void Step(FemElastoDynamics<TElasticEnergy>& fem, Params const& params, ChebyshevParams& cheb)
+void InitializeSolve(
+    FemElastoDynamics<TElasticEnergy>& fem,
+    Params const& params,
+    ChebyshevParams& cheb)
+{
+    PBAT_PROFILE_NAMED_SCOPE("pbat.sim.algorithm.vbd.Chebyshev.InitializeSolve");
+    InitializeSolve<TElasticEnergy>(fem, params);
+}
+
+template <physics::CHyperElasticEnergy TElasticEnergy>
+void Step(
+    Index k,
+    FemElastoDynamics<TElasticEnergy>& fem,
+    Params const& params,
+    ChebyshevParams& cheb)
 {
     PBAT_PROFILE_NAMED_SCOPE("pbat.sim.algorithm.vbd.Chebyshev.Step");
     Scalar rho2 = cheb.rho * cheb.rho;
-    Step(fem, params);
+    Step(k, fem, params);
     // Chebyshev Update
-    cheb.omega = kernels::ChebyshevOmega(cheb.k, rho2, cheb.omega);
+    cheb.omega = kernels::ChebyshevOmega(k, rho2, cheb.omega);
     auto& xk   = fem.x;
-    if (cheb.k > 1)
+    if (k > 1)
         xk = cheb.omega * (xk - cheb.xkm2) + cheb.xkm2;
     cheb.xkm2 = cheb.xkm1;
     cheb.xkm1 = xk;
