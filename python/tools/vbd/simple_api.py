a7f19ae4547d1381e5c50cedc34570f0521ebe29
Melissa2661 <melissa.katz@mail.mcgill.ca>
2025-11-13 15:26:55 -0500

    added saving an original mask before transforms potentially release them

--- python/tools/vbd/simple_api.py
+++ python/tools/vbd/simple_api.py
@@ -9,6 +9,7 @@ from tkinter import filedialog
 import h5py
 
 import utils.transform_library as tlib
+from utils.transform_library import TransformType
 
 
 def _read_mesh_and_state(integrate_grp: h5py.Group):
@@ -387,6 +388,7 @@ if __name__ == "__main__":
     
     transform_library = tlib.TransformLibrary()
     transform_library.deserialize("primitive_transforms.h5")
+    original_mask = None
 
     def callback():
         global Y, nu, rho, aext, b, v0, d_axis, d_percent, d_extremity, d_nodes
@@ -396,7 +398,7 @@ if __name__ == "__main__":
         global solver_names, i_solver, i_init_strategy, n_max_iters
         global i_broyden_l2_solver, i_broyden_jacobian_estimate
         global animate, export, t, vm, dpc
-        global archive, archive_path, archive_flush_period, transform_library
+        global archive, archive_path, archive_flush_period, transform_library, original_mask
 
         dirty = False
         is_new_mesh = False
@@ -427,8 +429,8 @@ if __name__ == "__main__":
                 root.withdraw()
                 file_path = filedialog.askopenfilename(
                     title="Open HDF5 simulation file",
-                    defaultextension=".h5",
-                    filetypes=[("HDF5 files", "*.h5;*.hdf5"), ("All files", "*.*")],
+                    defaultextension="*.*",
+                    filetypes=[("All files", "*.*"), ("HDF5 files", "*.h5;*.hdf5")],
                 )
                 if file_path:
                     h5 = h5py.File(file_path, "r")
@@ -440,6 +442,7 @@ if __name__ == "__main__":
                     C = E.T  # to shape (m,4)
                     dynamics.construct(V.T, C.T)
                     dynamics.constrain(d_mask.ravel())
+                    original_mask = d_mask.copy()
                     #dynamics.set_elastic_energy(dynamics.E, dynamics.wgU, dynamics.X, dynamics.lamegU[0], dynamics.lamegU[1])
                     element = pbat.fem.Element.Tetrahedron
                     order = 1  # linear shape functions only
@@ -717,6 +720,7 @@ if __name__ == "__main__":
                 x0,
                 xdot0,
             )
+            dynamics.constrain(original_mask.ravel() if original_mask is not None else np.zeros(n_nodes, dtype=int))
             t = 0
 
         # Simulate
@@ -731,10 +735,15 @@ if __name__ == "__main__":
                     frame_group = None
                     print(f"Archive group error: {e}")
             for transform in transform_library.transforms:
-                transformed_v = transform.apply(t, dt, dynamics.x)
-                for i in range(dynamics.x.shape[1]):
-                    if dynamics.dmask[i] == transform.id:
-                        dynamics.x[:, i] = transformed_v[:, i]
+                v = dynamics.x[:, dynamics.dmask == transform.id]
+                transformed_v = transform.apply(t, dt, v)
+                dynamics.x[:, dynamics.dmask == transform.id] = transformed_v
+                if transform.expired(t * dt) and transform.transform_type == TransformType.FIXED:
+                    dynamics.dmask[dynamics.dmask == transform.id] = 0
+                    dynamics.constrain(dynamics.dmask.ravel())
+                # for i in range(dynamics.x.shape[1]):
+                #     if dynamics.dmask[i] == transform.id:
+                #         dynamics.x[:, i] = transformed_v[:, i]
                 
             if i_solver == 0:
                 vbd_integrate(dynamics, vbd_params, archive=frame_group)
