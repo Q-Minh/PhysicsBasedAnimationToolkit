c5b05916369786b93d9adb2145220e87933a18bd
Q-Minh <tonthat.quocminh@gmail.com>
2025-12-04 16:32:11 -0500

    Refactor convergence plot options: remove unused flags and simplify flag handling

--- python/tools/vbd/ui/convergence.py
+++ python/tools/vbd/ui/convergence.py
@@ -7,44 +7,12 @@ from .solvers.base import BaseSolver
 import numpy as np
 
 
-plot_flags = [
-    # ("NoLabel", implot.ImPlotAxisFlags_NoLabel),
-    ("NoGridLines", implot.ImPlotAxisFlags_NoGridLines),
-    ("NoTickMarks", implot.ImPlotAxisFlags_NoTickMarks),
-    ("NoTickLabels", implot.ImPlotAxisFlags_NoTickLabels),
-    # ("NoInitialFit", implot.ImPlotAxisFlags_NoInitialFit),
-    # ("NoMenus", implot.ImPlotAxisFlags_NoMenus),
-    # ("NoSideSwitch", implot.ImPlotAxisFlags_NoSideSwitch),
-    # ("NoHighlight", implot.ImPlotAxisFlags_NoHighlight),
-    # ("Opposite", implot.ImPlotAxisFlags_Opposite),
-    # ("Foreground", implot.ImPlotAxisFlags_Foreground),
-    # ("Invert", implot.ImPlotAxisFlags_Invert),
-    ("AutoFit", implot.ImPlotAxisFlags_AutoFit),
-    # ("RangeFit", implot.ImPlotAxisFlags_RangeFit),
-    # ("PanStretch", implot.ImPlotAxisFlags_PanStretch),
-    # ("LockMin", implot.ImPlotAxisFlags_LockMin),
-    # ("LockMax", implot.ImPlotAxisFlags_LockMax),
-    # ("Lock", implot.ImPlotAxisFlags_Lock),
-    # ("NoDecorations", implot.ImPlotAxisFlags_NoDecorations),
-    # ("AuxDefault", implot.ImPlotAxisFlags_AuxDefault),
-]
-
-
-def reduce_flags(selected_flags: list[bool]) -> int:
-    flags = implot.ImPlotAxisFlags_None
-    for i, selected in enumerate(selected_flags):
-        if selected:
-            flags |= plot_flags[i][1]
-    return flags
-
-
 class Convergence:
     _f: list[list[float]]
     _gnorm2: list[list[float]]
     _errors: list[list[float]]
     _solver_names: list[str]
     _convergence_analysis_requested: bool
-    _plot_flag_mask: list[bool]
 
     def __init__(self):
         self._f = []
@@ -52,21 +20,15 @@ class Convergence:
         self._errors = []
         self._solver_names = []
         self._convergence_analysis_requested = False
-        self._plot_flag_mask = [False for _ in plot_flags]
 
     def draw(self):
         default_button_size = [imgui.GetWindowWidth() / 2.1, 0]
         imgui.PushID("Convergence")
-        if imgui.TreeNode("Plot Options"):
-            for i, (flag_name, flag_value) in enumerate(plot_flags):
-                _, self._plot_flag_mask[i] = imgui.Checkbox(
-                    flag_name,
-                    self._plot_flag_mask[i],
-                )
-            imgui.TreePop()
         if imgui.Button("Step", default_button_size):
             self._convergence_analysis_requested = True
-        flags = reduce_flags(self._plot_flag_mask)
+        flags = implot.ImPlotAxisFlags_None
+        if imgui.Button("Fit Axes", default_button_size):
+            flags = flags + implot.ImPlotAxisFlags_AutoFit
         if implot.BeginPlot("Objective"):
             implot.SetupAxes(
                 "Iteration",
