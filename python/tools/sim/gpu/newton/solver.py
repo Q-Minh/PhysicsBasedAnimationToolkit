1872ef58e2a7ae7343eb69658f67e94438293df4
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2026-06-04 16:42:15 -0400

    Partially implement Newton solver with CG solve and back-tracking line search.
    Need to revisit many sections to correctly implement.

--- python/tools/sim/gpu/newton/solver.py
+++ python/tools/sim/gpu/newton/solver.py
@@ -9,6 +9,7 @@ to the VBD solver.
 import warp as wp
 import warp.sparse
 import warp.optim.linear
+import cupy as cp
 
 from pbatoolkit import pbat
 from ..contact.mesh.cd import ContactDetection
@@ -18,7 +19,11 @@ from ..contact.dynamics import (
     MeshDynamicsData as ContactDynamicsData,
     PenaltyAdaptivity,
 )
-from ..elasticity import snh
+from ..elasticity.snh import snh_eval, snh_hess
+from ..elasticity.chain import hessian_block_wrt_dofs
+from ..gradient import Gradient
+from ..contact import halfedges
+from .. import types
 
 
 @wp.struct
@@ -47,6 +52,251 @@ class ParamsData:
     Hrows: wp.array[wp.int32]
     Hcols: wp.array[wp.int32]
     Hvals: wp.array[wp.mat33f]
+
+
+@wp.kernel
+def _energy_elastic(
+    fem: FemElastoDynamicsData,
+    h2: wp.float32,
+    energy: wp.array[wp.float32],
+):
+    e = wp.tid()
+    nodes = fem.E[e]
+    wge = fem.wg[e]
+    GP = fem.GNeg[e]
+    mu = fem.mug[e]
+    llambda = fem.lambdag[e]
+    xe = types.mat3x4f()
+    for j in range(4):
+        xj = fem.x[nodes[j]]
+        for d in range(3):
+            xe[d, j] = xj[d]
+    F = xe @ GP
+    wp.atomic_add(energy, 0, h2 * wge * snh_eval(F, mu, llambda))
+
+
+@wp.kernel
+def _energy_inertial(
+    fem: FemElastoDynamicsData,
+    energy: wp.array[wp.float32],
+):
+    i = wp.tid()
+    if is_dirichlet_node(fem.dmask, i):
+        return
+    diff = fem.x[i] - fem.xtilde[i]
+    wp.atomic_add(energy, 0, wp.float32(0.5) * fem.m[i] * wp.dot(diff, diff))
+
+
+@wp.kernel
+def _energy_vv(
+    x: wp.array[wp.vec3f],
+    xt: wp.array[wp.vec3f],
+    contact: ContactDynamicsData,
+    n_u: wp.int32,
+    energy: wp.array[wp.float32],
+):
+    c = wp.tid()
+    if wp.uint64(c) >= contact.contacts.vv.prefix[n_u]:
+        return
+    u, v = contact.contacts.vv.u[c], contact.contacts.vv.v[c]
+    n, t, bvec = (
+        contact.contacts.vv_bases.n[c],
+        contact.contacts.vv_bases.t[c],
+        contact.contacts.vv_bases.b[c],
+    )
+    i, j = contact.meshes.V[u], contact.meshes.V[v]
+    xi, xj, xti, xtj = x[i], x[j], xt[i], xt[j]
+    c_n = wp.dot(xi - xj, n) - contact.dmin - contact.cvv.s[c]
+    du = (xi - xti) - (xj - xtj)
+    c_f = wp.vec2f(wp.dot(du, t), wp.dot(du, bvec))
+    sigma_n = contact.gamma_n * contact.sigma_n[0]
+    sigma_f = contact.gamma_f * contact.sigma_f[0]
+    E = contact.cvv.gamma[c] * (
+        wp.float32(0.5) * sigma_n * c_n * c_n
+        - contact.cvv.lambda_n[c] * c_n
+        + wp.float32(0.5) * sigma_f * wp.dot(c_f, c_f)
+        - wp.dot(contact.cvv.lambda_f[c], c_f)
+    )
+    wp.atomic_add(energy, 0, E)
+
+
+@wp.kernel
+def _energy_ve(
+    x: wp.array[wp.vec3f],
+    xt: wp.array[wp.vec3f],
+    contact: ContactDynamicsData,
+    n_u: wp.int32,
+    energy: wp.array[wp.float32],
+):
+    c = wp.tid()
+    if wp.uint64(c) >= contact.contacts.ve.prefix[n_u]:
+        return
+    vi = contact.contacts.ve.u[c]
+    he = contact.contacts.ve.v[c]
+    n, t, bvec = (
+        contact.contacts.ve_bases.n[c],
+        contact.contacts.ve_bases.t[c],
+        contact.contacts.ve_bases.b[c],
+    )
+    b1 = contact.contacts.ve_bary[c]
+    b0 = wp.float32(1) - b1
+    i = contact.meshes.V[vi]
+    ea = halfedges.incoming_vertex(contact.meshes.F, he)
+    eb = halfedges.outgoing_vertex(contact.meshes.F, he)
+    xi, xti = x[i], xt[i]
+    xcp2 = b0 * x[ea] + b1 * x[eb]
+    xtcp2 = b0 * xt[ea] + b1 * xt[eb]
+    c_n = wp.dot(xi - xcp2, n) - contact.dmin - contact.cve.s[c]
+    du = (xi - xti) - (xcp2 - xtcp2)
+    c_f = wp.vec2f(wp.dot(du, t), wp.dot(du, bvec))
+    sigma_n = contact.gamma_n * contact.sigma_n[0]
+    sigma_f = contact.gamma_f * contact.sigma_f[0]
+    E = contact.cve.gamma[c] * (
+        wp.float32(0.5) * sigma_n * c_n * c_n
+        - contact.cve.lambda_n[c] * c_n
+        + wp.float32(0.5) * sigma_f * wp.dot(c_f, c_f)
+        - wp.dot(contact.cve.lambda_f[c], c_f)
+    )
+    wp.atomic_add(energy, 0, E)
+
+
+@wp.kernel
+def _energy_vf(
+    x: wp.array[wp.vec3f],
+    xt: wp.array[wp.vec3f],
+    contact: ContactDynamicsData,
+    n_u: wp.int32,
+    energy: wp.array[wp.float32],
+):
+    c = wp.tid()
+    if wp.uint64(c) >= contact.contacts.vf.prefix[n_u]:
+        return
+    vi = contact.contacts.vf.u[c]
+    f = contact.contacts.vf.v[c]
+    n, t, bvec = (
+        contact.contacts.vf_bases.n[c],
+        contact.contacts.vf_bases.t[c],
+        contact.contacts.vf_bases.b[c],
+    )
+    uv = contact.contacts.vf_bary[c]
+    b1, b2 = uv[0], uv[1]
+    b0 = wp.float32(1) - b1 - b2
+    i = contact.meshes.V[vi]
+    finds = contact.meshes.F[f]
+    xi, xti = x[i], xt[i]
+    xcp2 = b0 * x[finds[0]] + b1 * x[finds[1]] + b2 * x[finds[2]]
+    xtcp2 = b0 * xt[finds[0]] + b1 * xt[finds[1]] + b2 * xt[finds[2]]
+    c_n = wp.dot(xi - xcp2, n) - contact.dmin - contact.cvf.s[c]
+    du = (xi - xti) - (xcp2 - xtcp2)
+    c_f = wp.vec2f(wp.dot(du, t), wp.dot(du, bvec))
+    sigma_n = contact.gamma_n * contact.sigma_n[0]
+    sigma_f = contact.gamma_f * contact.sigma_f[0]
+    E = contact.cvf.gamma[c] * (
+        wp.float32(0.5) * sigma_n * c_n * c_n
+        - contact.cvf.lambda_n[c] * c_n
+        + wp.float32(0.5) * sigma_f * wp.dot(c_f, c_f)
+        - wp.dot(contact.cvf.lambda_f[c], c_f)
+    )
+    wp.atomic_add(energy, 0, E)
+
+
+@wp.kernel
+def _energy_ee(
+    x: wp.array[wp.vec3f],
+    xt: wp.array[wp.vec3f],
+    contact: ContactDynamicsData,
+    n_u: wp.int32,
+    energy: wp.array[wp.float32],
+):
+    c = wp.tid()
+    if wp.uint64(c) >= contact.contacts.ee.prefix[n_u]:
+        return
+    he_u = contact.contacts.ee.u[c]
+    he_v = contact.contacts.ee.v[c]
+    n, t, bvec = (
+        contact.contacts.ee_bases.n[c],
+        contact.contacts.ee_bases.t[c],
+        contact.contacts.ee_bases.b[c],
+    )
+    st = contact.contacts.ee_bary[c]
+    b1, b3 = st[0], st[1]
+    b0, b2 = wp.float32(1) - b1, wp.float32(1) - b3
+    ia = halfedges.incoming_vertex(contact.meshes.F, he_u)
+    ib = halfedges.outgoing_vertex(contact.meshes.F, he_u)
+    ic = halfedges.incoming_vertex(contact.meshes.F, he_v)
+    id_ = halfedges.outgoing_vertex(contact.meshes.F, he_v)
+    xcp1 = b0 * x[ia] + b1 * x[ib]
+    xtcp1 = b0 * xt[ia] + b1 * xt[ib]
+    xcp2 = b2 * x[ic] + b3 * x[id_]
+    xtcp2 = b2 * xt[ic] + b3 * xt[id_]
+    c_n = wp.dot(xcp1 - xcp2, n) - contact.dmin - contact.cee.s[c]
+    du = (xcp1 - xtcp1) - (xcp2 - xtcp2)
+    c_f = wp.vec2f(wp.dot(du, t), wp.dot(du, bvec))
+    sigma_n = contact.gamma_n * contact.sigma_n[0]
+    sigma_f = contact.gamma_f * contact.sigma_f[0]
+    E = contact.cee.gamma[c] * (
+        wp.float32(0.5) * sigma_n * c_n * c_n
+        - contact.cee.lambda_n[c] * c_n
+        + wp.float32(0.5) * sigma_f * wp.dot(c_f, c_f)
+        - wp.dot(contact.cee.lambda_f[c], c_f)
+    )
+    wp.atomic_add(energy, 0, E)
+
+
+@wp.kernel
+def _fill_hessian_values(
+    fem: FemElastoDynamicsData,
+    h2: wp.float32,
+    params: ParamsData,
+):
+    tid = wp.tid()
+    n_nodes = fem.x.shape[0]
+    n_elems = fem.E.shape[0]
+    identity = wp.identity(n=3, dtype=wp.float32)
+    if tid < n_nodes:
+        i = tid
+        if is_dirichlet_node(fem.dmask, i):
+            params.Hvals[i] = identity
+        else:
+            params.Hvals[i] = fem.m[i] * identity
+    if tid < n_elems:
+        e = tid
+        offset = n_nodes
+        nodes = fem.E[e]
+        wge = fem.wg[e]
+        GP = fem.GNeg[e]
+        mu = fem.mug[e]
+        llambda = fem.lambdag[e]
+        xe = types.mat3x4f()
+        for j in range(4):
+            xj = fem.x[nodes[j]]
+            for d in range(3):
+                xe[d, j] = xj[d]
+        F = xe @ GP
+        HF = snh_hess(F, mu, llambda)
+        for ii in range(4):
+            for jj in range(4):
+                idx = offset + e * 16 + ii * 4 + jj
+                if is_dirichlet_node(fem.dmask, nodes[ii]) or is_dirichlet_node(
+                    fem.dmask, nodes[jj]
+                ):
+                    params.Hvals[idx] = wp.mat33f(wp.float32(0))
+                else:
+                    params.Hvals[idx] = (
+                        h2 * wge * hessian_block_wrt_dofs(HF, GP, ii, jj)
+                    )
+
+
+@wp.kernel
+def _negate_array(a: wp.array[wp.vec3f], out: wp.array[wp.vec3f]):
+    i = wp.tid()
+    out[i] = -a[i]
+
+
+@wp.kernel
+def _axpy(y: wp.array[wp.vec3f], x: wp.array[wp.vec3f], alpha: wp.float32):
+    i = wp.tid()
+    y[i] = y[i] + alpha * x[i]
 
 
 @wp.kernel
@@ -141,10 +391,12 @@ class Params:
         self._data.Hrows = wp.zeros(n_max_triplets, dtype=wp.int32)
         self._data.Hcols = wp.zeros(n_max_triplets, dtype=wp.int32)
         self._data.Hvals = wp.zeros(n_max_triplets, dtype=wp.mat33f)
+        self._data.GVGp = wp.zeros(1, dtype=wp.int32)
+        self._data.GVGadj = wp.zeros(1, dtype=wp.int32)
         # Create template hessian triplets
         wp.launch(
             kernel=_initialize_hessian_triplets,
-            dim=n_elems,
+            dim=max(n_nodes, n_elems),
             inputs=[fem.data, self._data],
         )
         self._H = warp.sparse.bsr_from_triplets(
@@ -153,8 +405,13 @@ class Params:
             self._data.Hrows,
             self._data.Hcols,
             self._data.Hvals,
-            prune_numerical_zeros=True,
+            prune_numerical_zeros=False,
         )
+        self._g = wp.zeros(n_nodes, dtype=wp.vec3f)
+        self._neg_g = wp.zeros(n_nodes, dtype=wp.vec3f)
+        self._dx = wp.zeros(n_nodes, dtype=wp.vec3f)
+        self._energy_buf = wp.zeros(1, dtype=wp.float32)
+        self._gradient = Gradient(fem, contact, self._g)
 
     @property
     def data(self) -> ParamsData:  # pyright: ignore[reportGeneralTypeIssues]
@@ -174,13 +431,41 @@ def initialize_solve(
     cd.filter_step()
 
 
+def _compute_energy(
+    fem: FemElastoDynamics,
+    contact: ContactDynamics,
+    params: Params,
+    h2: wp.float32,
+) -> float:
+    params._energy_buf.zero_()
+    n_nodes = fem.data.x.shape[0]
+    n_elems = fem.data.E.shape[0]
+    cd = contact._data
+    x, xt = fem.data.x, fem.data.xt
+    wp.launch(_energy_elastic, dim=n_elems, inputs=[fem.data, h2, params._energy_buf])
+    wp.launch(_energy_inertial, dim=n_nodes, inputs=[fem.data, params._energy_buf])
+    for kernel, cs in [
+        (_energy_vv, contact.cvv),
+        (_energy_ve, contact.cve),
+        (_energy_vf, contact.cvf),
+        (_energy_ee, contact.cee),
+    ]:
+        wp.launch(
+            kernel, dim=cs.capacity, inputs=[x, xt, cd, cs.n_u, params._energy_buf]
+        )
+    wp.synchronize()
+    return float(params._energy_buf.numpy()[0])
+
+
 def check_convergence(
     fem: FemElastoDynamics,
     contact: ContactDynamics,
     params: Params,
 ) -> bool:
-    """TODO: return True when ||gradient||^2 <= gtol2."""
-    return False
+    params._gradient.compute()
+    g_cp = cp.asarray(params._g).ravel()
+    gnorm2 = float(cp.dot(g_cp, g_cp))
+    return gnorm2 <= float(params.data.gtol2)
 
 
 def prepare_subproblem(
@@ -189,8 +474,7 @@ def prepare_subproblem(
     cd: ContactDetection,
     params: Params,
 ) -> None:
-    """TODO: assemble Hessian, adapt penalty parameters."""
-    pass
+    contact.adapt_penalty_parameters()
 
 
 def solve_subproblem(
@@ -198,8 +482,67 @@ def solve_subproblem(
     contact: ContactDynamics,
     params: Params,
 ) -> None:
-    """TODO: Newton solve where each iteration is a Jacobi-preconditioned CG linear solve + backtracking line search."""
-    pass
+    n_nodes = fem.data.x.shape[0]
+    n_elems = fem.data.E.shape[0]
+    h2 = fem.bdf.beta_tilde**2
+    params._gradient.compute()
+    for _ in range(int(params.data.n_subproblem_max_iters)):
+        g_cp = cp.asarray(params._g).ravel()
+        gnorm2 = float(cp.dot(g_cp, g_cp))
+        if gnorm2 <= float(params.data.gtol2):
+            break
+        # Assemble FEM + inertial Hessian
+        wp.launch(
+            _fill_hessian_values,
+            dim=max(n_nodes, n_elems),
+            inputs=[fem.data, h2, params._data],
+        )
+        warp.sparse.bsr_set_from_triplets(
+            dest=params._H,
+            rows=params._data.Hrows,
+            columns=params._data.Hcols,
+            values=params._data.Hvals,
+            count=None,
+            prune_numerical_zeros=True,
+            masked=False,
+        )
+        M = warp.optim.linear.preconditioner(params._H, "diag")
+        # Solve H dx = -g
+        params._dx.zero_()
+        final_iteration, residual_norm, absolute_tolerance = warp.optim.linear.cg(
+            params._H,
+            params._g,
+            x=params._dx,
+            tol=1e-5,
+            atol=None,
+            maxiter=300,
+            M=M,
+            use_cuda_graph=True,
+        )
+        # Check descent direction
+        dx_cp = cp.asarray(params._dx).ravel()
+        slope = float(cp.dot(g_cp, dx_cp))
+        if slope >= 0.0:
+            break
+        # Armijo backtracking line search
+        E0 = _compute_energy(fem, contact, params, h2)
+        alpha = float(params.data.ls_alpha)
+        x_backup = wp.clone(fem.data.x)
+        accepted = False
+        for _ in range(int(params.data.ls_max_iters)):
+            wp.copy(fem.data.x, x_backup)
+            wp.launch(
+                _axpy, dim=n_nodes, inputs=[fem.data.x, params._dx, wp.float32(alpha)]
+            )
+            E_trial = _compute_energy(fem, contact, params, h2)
+            if E_trial <= E0 + float(params.data.ls_c) * alpha * slope:
+                accepted = True
+                break
+            alpha *= float(params.data.ls_tau)
+        if not accepted:
+            wp.copy(fem.data.x, x_backup)
+            break
+        params._gradient.compute()
 
 
 def finalize_subproblem(
